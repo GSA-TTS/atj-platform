67a197605bd3468f0e1710aa66731cbc9f86cf4c
Daniel Naab <dan@crushingpennies.com>
2023-06-19 12:53:47 -0500

    Testing ajv-based validation

--- packages/interviews/src/services/interview-service.test.ts
+++ packages/interviews/src/services/interview-service.test.ts
@@ -5,8 +5,21 @@ import { InterviewService } from './interview-service';
 describe('interview service', () => {
   const interviewService = new InterviewService();
 
-  it('works', () => {
-    const response = interviewService.helloWorld();
-    expect(response).toEqual('Hello, world!');
+  it('returns errors for missing required fields', async () => {
+    const result = await interviewService.validateInterview({
+      address: '123 N. Main St.',
+      city: 'Small Town',
+      //state: 'WI',
+      postalCode: '00000',
+    });
+    expect(result).toEqual([
+      {
+        instancePath: '',
+        schemaPath: '#/required',
+        keyword: 'required',
+        params: { missingProperty: 'state' },
+        message: "must have required property 'state'",
+      },
+    ]);
   });
 });
--- packages/interviews/src/services/interview-service.ts
+++ packages/interviews/src/services/interview-service.ts
@@ -1,7 +1,46 @@
-export class InterviewService {
-  constructor() {}
+import Ajv, { JSONSchemaType } from 'ajv';
+
+const ajv = new Ajv();
+
+type InterviewError = {
+  field: string;
+  message: string;
+};
+
+const addressSchema = {
+  //$schema: 'https://json-schema.org/draft/2020-12/schema',
+  type: 'object',
+  properties: {
+    address: { type: 'string' },
+    city: { type: 'string' },
+    state: { type: 'string', enum: ['MN', 'WI'] },
+    postalCode: { type: 'string', pattern: '^\\d{5}(?:[-\\s]\\d{4})?$' },
+  },
+  required: ['address', 'city', 'state', 'postalCode'],
+  additionalProperties: false,
+};
+
+export class InterviewService<T> {
+  constructor(schema: typeof addressSchema = addressSchema) {}
 
   helloWorld(): string {
     return 'Hello, world!';
+  }
+
+  validateInterview(interview: any) {
+    const validate = ajv.compile(addressSchema);
+    validate(interview);
+    /* example
+    [
+      {
+        instancePath: '',
+        schemaPath: '#/required',
+        keyword: 'required',
+        params: { missingProperty: 'state' },
+        message: "must have required property 'state'"
+      }
+    ]
+    */
+    return validate.errors;
   }
 }
