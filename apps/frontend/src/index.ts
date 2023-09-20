4aa1c9dfd74a69e9b4b4452645f65917ef939646
Daniel Naab <dan@crushingpennies.com>
2023-09-20 15:16:50 -0500

    Add a footer and links to the Github repository

--- apps/frontend/src/index.ts
+++ apps/frontend/src/index.ts
@@ -1,7 +1,8 @@
-import { createAppRoot } from './views/main';
+import { createAppRoot } from './main';
 
 createAppRoot(document.getElementById('root') as HTMLElement, {
   backend: {
     helloWorld: (str: string) => str,
   },
+  github: import.meta.env.GITHUB,
 });
