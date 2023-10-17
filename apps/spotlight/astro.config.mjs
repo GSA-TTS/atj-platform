b1ec6a74f4d44d715ba6e6c129ea3b53e6da40a7
Daniel Naab <dan@crushingpennies.com>
2023-10-17 12:56:13 -0500

    React components rendering.

--- apps/spotlight/astro.config.mjs
+++ apps/spotlight/astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    react({
+      include: ['src/components/**'],
+    }),
+  ],
+});
