ce7149b91e6ba6a229d4c438f41e23831b272e43
Daniel Naab <dan@crushingpennies.com>
2023-10-18 10:49:30 -0500

    React and Astro components split up and functional

--- apps/spotlight/astro.config.mjs
+++ apps/spotlight/astro.config.mjs
@@ -1,11 +1,32 @@
+import { execSync } from 'child_process';
+
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+
+const BASEURL = process.env.BASEURL || '/';
+const GITHUB = {
+  owner: process.env.OWNER || 'gsa-tts',
+  repository: process.env.REPOSITORY || 'atj-platform',
+  branch: process.env.BRANCH || 'main',
+  commit: execSync('git rev-parse HEAD').toString().trim(),
+};
+
+const DEPLOYMENT_ID =
+  process.env.NODE_ENV === 'development'
+    ? 'local'
+    : `${GITHUB.owner}:${GITHUB.branch}`;
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     react({
-      include: ['src/components/**'],
+      include: ['src/components/react/**'],
     }),
   ],
+  vite: {
+    define: {
+      'import.meta.env.DEPLOYMENT_ID': JSON.stringify(DEPLOYMENT_ID),
+      'import.meta.env.GITHUB': JSON.stringify(GITHUB),
+    },
+  },
 });
--- apps/spotlight/src/components/header.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-//import closeSvg from '@uswds/img/usa-icons/close.svg';
-
-export const Header = () => {
-  return (
-    <header className="usa-header usa-header--basic usa-header--megamenu"></header>
-  );
-};
