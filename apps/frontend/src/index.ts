61a6d999874ea4b50d26ac54c1e0ed1f1edc7ea0
Daniel Naab <dan@crushingpennies.com>
2023-10-18 10:50:12 -0500

    Remove React starter frontend

--- apps/frontend/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { createAppRoot } from './main';
-
-createAppRoot(document.getElementById('root') as HTMLElement, {
-  backend: {
-    helloWorld: (str: string) => str,
-  },
-  github: import.meta.env.GITHUB,
-});
