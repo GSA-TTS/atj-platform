0818ef68d2286fb270981ec27eb7d13a49528445
Daniel Naab <dan@crushingpennies.com>
2023-10-18 10:12:25 -0500

    Add React components from prior site

---
+++ apps/spotlight/src/components/header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+//import closeSvg from '@uswds/img/usa-icons/close.svg';
+
+export const Header = () => {
+  return (
+    <header className="usa-header usa-header--basic usa-header--megamenu"></header>
+  );
+};
