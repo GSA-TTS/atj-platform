2b578bcfa8b03324382e3b7e73e37291d0becd2f
Daniel Naab <dan@crushingpennies.com>
2024-01-02 16:01:21 -0600

    Put field details in a grid

--- apps/spotlight/src/components/react/form-builder.tsx
+++ apps/spotlight/src/components/react/form-builder.tsx
@@ -3,51 +3,50 @@ import React from 'react';
 import { SuggestedForm } from '@atj/documents/src/suggestions';
 
 export const FormBuilder = ({ fields }: { fields: SuggestedForm }) => {
-  return fields.map((field, index) => {
-    return <FieldBuilder key={index} field={field} />;
-  });
+  return (
+    <div>
+      {fields.map((field, index) => {
+        return <FieldBuilder key={index} field={field} />;
+      })}
+    </div>
+  );
 };
 
 const FieldBuilder = ({ field }: { field: SuggestedForm[number] }) => {
   const fieldId = `field-${field.id}`;
   return (
-    <fieldset className="usa-fieldset">
-      <dl>
-        <dt>id</dt>
-        <dd>{field.id}</dd>
-        <dt>name</dt>
-        <dd>{field.name}</dd>
-      </dl>
-      <select className="usa-select" name={fieldId}>
-        <option value={'input'}>Input</option>
-        <option value={'textarea'}>Textarea</option>
-      </select>
-      <label className="usa-label">
-        <input
-          className="usa-input"
-          type="text"
-          name={`input-type-${fieldId}`}
-          defaultValue={field.value}
-        ></input>
-      </label>
-      <label className="usa-label">
-        Field label
-        <input
-          className="usa-input"
-          type="text"
-          name={`description-${fieldId}`}
-          defaultValue={field.label}
-        ></input>
-      </label>
-      <label className="usa-label">
-        Default value
-        <input
-          className="usa-input"
-          type="text"
-          name={`default-value-${fieldId}`}
-          defaultValue={field.value}
-        ></input>
-      </label>
-    </fieldset>
+    <div className="grid-row grid-gap">
+      <div className="grid-col">
+        <label className="usa-label">
+          Input type
+          <select className="usa-select" name={`input-type-${fieldId}`}>
+            <option value={'input'}>Input</option>
+            <option value={'textarea'}>Textarea</option>
+          </select>
+        </label>
+      </div>
+      <div className="grid-col">
+        <label className="usa-label">
+          Field label
+          <input
+            className="usa-input"
+            type="text"
+            name={`description-${fieldId}`}
+            defaultValue={field.label}
+          ></input>
+        </label>
+      </div>
+      <div className="grid-col">
+        <label className="usa-label">
+          Default value
+          <input
+            className="usa-input"
+            type="text"
+            name={`default-value-${fieldId}`}
+            defaultValue={field.value}
+          ></input>
+        </label>
+      </div>
+    </div>
   );
 };
