4416ab57fe7dc1c719c502dc8dcf1708215f2d6b
Daniel Naab <dan@crushingpennies.com>
2024-01-19 12:13:13 -0600

    log env var

--- vitest.shared.ts
+++ vitest.shared.ts
@@ -1,11 +1,16 @@
+import { env } from 'node:process';
+
 import type { UserConfig } from 'vitest/config';
 const {
   default: GithubActionsReporter,
 } = require('vitest-github-actions-reporter');
 
+console.log('****');
+console.log(env.GITHUB_ACTIONS);
+
 export default {
   test: {
-    reporters: process.env.GITHUB_ACTIONS
+    reporters: env.GITHUB_ACTIONS
       ? ['default', new GithubActionsReporter()]
       : 'default',
   },
