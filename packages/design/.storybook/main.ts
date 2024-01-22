068ee4f95c1826195c0162c9ef025ae445e1f08b
Daniel Naab <dan@crushingpennies.com>
2024-01-22 15:37:17 -0600

    Add Storybook a11y plugin, which has Axe integrated.

--- packages/design/.storybook/main.ts
+++ packages/design/.storybook/main.ts
@@ -17,6 +17,7 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-a11y'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-vite') as '@storybook/react-vite',
