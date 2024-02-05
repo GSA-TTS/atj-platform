39c71b6eadec93073b4ac02fa3d91ce962e452f6
Daniel Naab <dan@crushingpennies.com>
2024-02-05 10:49:07 -0600

    Remove old "interviews" package, which was replaced by "forms", and which has only remained checked in for reference.

--- packages/interviews/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './fact';
-export { type Interview } from './interview';
-export { createFormContext, nextContext } from './interview-context';
-export * from './prompt';
-export {
-  createSequentialInterview,
-  type SequentialInterview,
-} from './strategies/sequential';
