5057d0095fc5b1b3de6d2fa0a53b22a6405bf96b
Natasha Pierre-Louis <natasha.pierre-louis@gsa.gov>
2024-02-20 12:51:51 -0700

    Update style of form builder (#47)
    * Phase 1 of styling for the Form Builder Tool
    
    * Updated the go to link on the homepage
    
    * Phase 1 of styling for the Form Builder Tool
    
    * Updated the go to link on the homepage
    
    * Removed the extra views of the edit form.
    
    * Comment out coverage report
    
    * Linting
    
    ---------
    
    Co-authored-by: Daniel Naab <dan@crushingpennies.com>

---
+++ packages/design/src/FormManager/internalPageTopNav.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { type FormService } from '@atj/form-service';
+
+export default function InnerPageTopNav({
+  formId,
+  formService,
+}: {
+  formId: string;
+  formService: FormService;
+}) {
+  const location = useLocation();
+  const result = formService.getForm(formId);
+  if (!result.success) {
+    return 'Form not found';
+  }
+  const isPreviewPage = location.pathname === `/${formId}`;
+  const isEditPage = location.pathname.includes(`/${formId}/edit`);
+  const isImportDocuments = location.pathname.includes(
+    `/${formId}/import-document`
+  );
+  return (
+    <div className="innerPageTopNavWrapper">
+      <div className="innerPageTopNav">
+        <ul className="grid-row">
+          {(isPreviewPage || isEditPage) && (
+            <li className={`grid-col ${isPreviewPage ? 'currentPage' : ''}`}>
+              <Link to={`/${formId}`}>Preview</Link>
+            </li>
+          )}
+          {(isPreviewPage || isEditPage || isImportDocuments) && (
+            <li className={`grid-col ${isEditPage ? 'currentPage' : ''}`}>
+              <Link to={`/${formId}/edit`}>
+                {isImportDocuments ? 'Back to Edit Page' : 'Edit'}
+              </Link>
+            </li>
+          )}
+          <li className={`grid-col ${isImportDocuments ? 'currentPage' : ''}`}>
+            <Link to={`/${formId}/import-document`}>Import</Link>
+          </li>
+          <li className="grid-col">
+            <Link to="/">Manage Forms</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
