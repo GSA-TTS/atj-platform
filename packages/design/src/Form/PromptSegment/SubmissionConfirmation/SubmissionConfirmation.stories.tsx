de8d2be2e51c7f115312fb9f850b084a299708f6
Daniel Naab <daniel.naab@gsa.gov>
2024-02-21 11:17:14 -0600

    Add missing Storybook story for SubmissionConfirmation. (#49)

---
+++ packages/design/src/Form/PromptSegment/SubmissionConfirmation/SubmissionConfirmation.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import SubmissionConfirmation, { type SubmissionConfirmationProps } from '.';
+
+export default {
+  title: 'prompts/SubmissionConfirmation',
+  component: SubmissionConfirmation,
+  tags: ['autodocs'],
+} satisfies Meta<typeof SubmissionConfirmation>;
+
+export const SubmissionConfirmationExample = {
+  args: {
+    prompt: {
+      type: 'submission-confirmation',
+      table: [
+        { label: 'Field 1', value: 'Value 1' },
+        { label: 'Field 2', value: 'Value 2' },
+        { label: 'Field 3', value: 'Value 3' },
+        { label: 'Field 4', value: 'Value 4' },
+      ],
+    },
+  } satisfies SubmissionConfirmationProps,
+} satisfies StoryObj<typeof SubmissionConfirmation>;
