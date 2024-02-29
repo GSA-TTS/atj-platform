3d033ccaf90cde42c9b56f6aa6977fe9732b4415
Daniel Naab <daniel.naab@gsa.gov>
2024-02-29 08:55:05 -0600

    Style the submission confirmation pattern (#57)

--- packages/design/src/config/view/SubmissionConfirmation/index.tsx
+++ packages/design/src/config/view/SubmissionConfirmation/index.tsx
@@ -11,24 +11,59 @@ const SubmissionConfirmation: FormElementComponent<
       <legend className="usa-legend usa-legend--large">
         Submission confirmation
       </legend>
-      <table>
-        <thead>
-          <tr>
-            <th>Field</th>
-            <th>Value</th>
-          </tr>
-        </thead>
-        <tbody>
-          {pattern.table.map((row, index) => {
-            return (
-              <tr key={index}>
-                <td>{row.label}</td>
-                <td>{row.value}</td>
+      <div className="usa-alert usa-alert--success margin-bottom-2">
+        <div className="usa-alert__body">
+          <h4 className="usa-alert__heading">Submission complete</h4>
+          <p className="usa-alert__text">
+            Thank you for submitting your filing. Your document package has been
+            auto-downloaded.
+          </p>
+        </div>
+      </div>
+      <h2>Next steps:</h2>
+      <div className="margin-bottom-4">
+        <ul className="usa-list">
+          <li>Review your document package for accuracy</li>
+          <li>Print your document package </li>
+          <li>Follow the instructions on the first page of your package</li>
+        </ul>
+      </div>
+      <div className="usa-accordion">
+        <h4 className="usa-accordion__heading">
+          <button
+            type="button"
+            className="usa-accordion__button"
+            aria-expanded={false}
+            aria-controls="submission-confirmation-table"
+          >
+            Submission details
+          </button>
+        </h4>
+        <div
+          id="submission-confirmation-table"
+          className="usa-accordion__content usa-prose"
+          hidden={true}
+        >
+          <table className="usa-table usa-table--striped width-full">
+            <thead>
+              <tr>
+                <th scope="col">Form field</th>
+                <th scope="col">Provided value</th>
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+              {pattern.table.map((row, index) => {
+                return (
+                  <tr key={index}>
+                    <th scope="row">{row.label}</th>
+                    <td>{row.value}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   );
 };
