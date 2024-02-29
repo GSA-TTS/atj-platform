beb835c3ef830e0a5e41baecac70c820f682bbd0
Jim Moffet <jimmoffet@gmail.com>
2024-02-28 20:48:54 -0800

    Updates to form styling for demo (#54)
    * Initial updates to preview form styling.
    
    * Demo tweaks
    
    ---------
    
    Co-authored-by: Jason Nakai <106776019+jasnakai@users.noreply.github.com>
    Co-authored-by: Daniel Naab <dan@crushingpennies.com>

--- packages/design/src/Form/index.tsx
+++ packages/design/src/Form/index.tsx
@@ -89,34 +89,161 @@ export default function Form({
     updatePrompt(allFormData);
   }, [allFormData]);
   */
+  console.log(JSON.stringify(prompt));
   return (
     <FormProvider {...formMethods}>
-      <form
-        className="previewForm"
-        onSubmit={formMethods.handleSubmit(async data => {
-          updatePrompt(data);
-          if (onSubmit) {
-            console.log('Submitting form...');
-            onSubmit(data);
-          } else {
-            console.warn('Skipping form submission...');
-          }
-        })}
-      >
-        <fieldset className="usa-fieldset">
-          {prompt.parts.map((part, index) => {
-            return (
-              <PromptComponent
-                key={index}
-                context={context}
-                promptPart={part}
-              />
-            );
-          })}
-          {/* Add submit button or other controls as needed */}
-        </fieldset>
-        <ActionBar actions={prompt.actions} />
-      </form>
+      <div className="preview">
+        <div className="grid-row">
+          <nav className="sideNav tablet:grid-col-3 margin-bottom-4 tablet:margin-bottom-0">
+            <ul className="usa-sidenav">
+              <li className="usa-sidenav__item">
+                <a href="">County</a>
+              </li>
+              <li className="usa-sidenav__item">
+                <a className="usa-current" href="">
+                  Current name
+                </a>
+              </li>
+              <ul className="usa-sidenav__sublist">
+                <li className="usa-sidenav__item">
+                  <a className="usa-current" href="">
+                    First name
+                  </a>
+                </li>
+                <li className="usa-sidenav__item">
+                  <a href="">Middle name</a>
+                </li>
+                <li className="usa-sidenav__item">
+                  <a href="">Last name</a>
+                </li>
+              </ul>
+              <li className="usa-sidenav__item">
+                <a href="">Declarations</a>
+              </li>
+              <li className="usa-sidenav__item">
+                <a href="">Address</a>
+              </li>
+              <li className="usa-sidenav__item">
+                <a href="">Telephone</a>
+              </li>
+              <li className="usa-sidenav__item">
+                <a href="">Date of Birth</a>
+              </li>
+              <li className="usa-sidenav__item">
+                <a href="">Name at Birth</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="grid-col-9 usa-prose">
+            <form
+              className="previewForm usa-form usa-form--large margin-bottom-3"
+              onSubmit={formMethods.handleSubmit(async data => {
+                updatePrompt(data);
+                if (onSubmit) {
+                  console.log('Submitting form...');
+                  onSubmit(data);
+                } else {
+                  console.warn('Skipping form submission...');
+                }
+              })}
+            >
+              {false && (
+                <fieldset className="usa-fieldset">
+                  <legend className="usa-legend usa-legend--large">
+                    Request to Change Name
+                  </legend>
+                  <div className="usa-form-group">
+                    <div className="usa-form-group">
+                      <fieldset className="usa-fieldset">
+                        <legend className="usa-legend usa-legend--large">
+                          County where you live
+                        </legend>
+                        <label className="usa-label">
+                          Name of your county *
+                        </label>
+                        <input
+                          className="usa-input"
+                          id="input-users1_address_line_one"
+                          name="users1_address_line_one"
+                          type="text"
+                          aria-describedby="input-message-users1_address_line_one"
+                          value=""
+                        />
+                      </fieldset>
+                    </div>
+                  </div>
+
+                  <div className="usa-form-group">
+                    <div className="usa-form-group">
+                      <fieldset className="usa-fieldset">
+                        <legend className="usa-legend usa-legend--large">
+                          Your current name
+                        </legend>
+                        <label className="usa-label">First name *</label>
+                        <input
+                          className="usa-input"
+                          id="input-users1_first_name"
+                          name="users1_first_name"
+                          type="text"
+                          aria-describedby="input-message-users1_first_name"
+                          value=""
+                        />
+                        <label className="usa-label">Middle name *</label>
+                        <input
+                          className="usa-input"
+                          id="input-users1_middle_name"
+                          name="users1_middle_name"
+                          type="text"
+                          aria-describedby="input-message-users1_middle_name"
+                          value=""
+                        />
+                        <label className="usa-label">Last name *</label>
+                        <input
+                          className="usa-input"
+                          id="input-users1_last_name"
+                          name="users1_last_name"
+                          type="text"
+                          aria-describedby="input-message-users1_last_name"
+                          value=""
+                        />
+                      </fieldset>
+                      <fieldset className="usa-fieldset">
+                        <p>
+                          To ask the court to change your name, you must fill
+                          out this form, and:
+                        </p>
+                        <ul>
+                          <li>
+                            Attach a certified copy of your birth certificate
+                            and a copy of your photo ID, and
+                          </li>
+                          <li>
+                            File your form and attachements in the same county
+                            where you live.
+                          </li>
+                        </ul>
+                      </fieldset>
+                    </div>
+                  </div>
+                </fieldset>
+              )}
+
+              <fieldset className="usa-fieldset">
+                {prompt.parts.map((part, index) => {
+                  return (
+                    <PromptComponent
+                      key={index}
+                      context={context}
+                      promptPart={part}
+                    />
+                  );
+                })}
+              </fieldset>
+              <ActionBar actions={prompt.actions} />
+            </form>
+          </div>
+        </div>
+      </div>
     </FormProvider>
   );
 }
