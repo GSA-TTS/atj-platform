a94a52e96ec204c09f26afa1251135cba9564e3d
Daniel Naab <dan@crushingpennies.com>
2024-03-05 10:14:49 -0600

    Tweak Astro to host static files correctly.

--- apps/doj-demo/astro.config.mjs
+++ apps/doj-demo/astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  trailingSlash: 'always',
+  output: 'server',
+  trailingSlash: 'never',
   base: addTrailingSlash(process.env.BASEURL || ''),
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [
     react({
       include: ['src/components/react/**'],
