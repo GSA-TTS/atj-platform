5982945d29c92eb27022b0cd104e796beef9b76f
Daniel Naab <dan@crushingpennies.com>
2024-03-20 10:39:33 -0500

    Add DOJ Pardon's marijuana pardon application form

--- packages/design/src/FormManager/FormList/PDFFileSelect/index.tsx
+++ packages/design/src/FormManager/FormList/PDFFileSelect/index.tsx
@@ -15,18 +15,24 @@ export default function PDFFileSelect({
   const { actions } = useDocumentImporter(formService, baseUrl);
   return (
     <div className="usa-form-group">
-      <label className="usa-label" id="file-input-specific-hint" htmlFor="file-input-specific">
+      <label
+        className="usa-label"
+        id="file-input-specific-hint"
+        htmlFor="file-input-specific"
+      >
         Select a single PDF file
       </label>
       <div className="usa-file-input">
-        <div className="usa-sr-only" aria-live="polite">No files selected.</div>
+        <div className="usa-sr-only" aria-live="polite">
+          No files selected.
+        </div>
         <div className="usa-file-input__target">
           <div className="usa-file-input__box"></div>
           <div className="usa-file-input__instructions" aria-hidden="true">
             Drag file here or{' '}
             <span className="usa-file-input__choose">choose from folder</span>
           </div>
-          
+
           <input
             className="usa-file-input__input"
             id="file-input-specific"
@@ -41,27 +47,38 @@ export default function PDFFileSelect({
       </div>
       <label className="usa-label">
         Or use an example file, selected for testing purposes:
-        <button
-          className="usa-button--unstyled"
-          onClick={async () => {
-            actions.stepOneSelectPdfByUrl(
-              'sample-documents/ca-unlawful-detainer/ud105.pdf'
-            );
-          }}
-        >
-          sample-documents/ca-unlawful-detainer/ud105.pdf
-        </button>
-        <button
-          className="usa-button--unstyled"
-          onClick={async () => {
-            actions.stepOneSelectPdfByUrl(
-              'sample-documents/alabama-name-change/ps-12.pdf'
-            );
-          }}
-        >
-          sample-documents/alabama-name-change/ps-12.pdf
-        </button>
+        <SampleDocumentButton
+          callback={actions.stepOneSelectPdfByUrl}
+          documentPath="sample-documents/ca-unlawful-detainer/ud105.pdf"
+        />
+        <SampleDocumentButton
+          callback={actions.stepOneSelectPdfByUrl}
+          documentPath="sample-documents/alabama-name-change/ps-12.pdf"
+        />
+        <SampleDocumentButton
+          callback={actions.stepOneSelectPdfByUrl}
+          documentPath="sample-documents/doj-pardon-marijuana/application_for_certificate_of_pardon_for_simple_marijuana_possession.pdf"
+        />
       </label>
     </div>
   );
 }
+
+const SampleDocumentButton = ({
+  callback,
+  documentPath,
+}: {
+  callback: (path: string) => Promise<void>;
+  documentPath: string;
+}) => {
+  return (
+    <button
+      className="usa-button--unstyled"
+      onClick={async () => {
+        await callback(documentPath);
+      }}
+    >
+      {documentPath}
+    </button>
+  );
+};
