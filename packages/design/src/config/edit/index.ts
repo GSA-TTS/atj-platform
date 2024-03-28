8e6fcf2f8eea4fe4dbd6b374f5dfef8b77d09173
Daniel Naab <dan@crushingpennies.com>
2024-03-28 16:00:11 -0500

    Add missing edit UI components

--- packages/design/src/config/edit/index.ts
+++ packages/design/src/config/edit/index.ts
@@ -1,10 +1,14 @@
+import FormSummaryEdit from './FormSummaryEdit';
 import InputPatternEdit from './InputPatternEdit';
+import ParagraphPatternEdit from './ParagraphPatternEdit';
 import SequencePatternEdit from './SequencePatternEdit';
 import SubmissionConfirmationEdit from './SubmissionConfirmationEdit';
 import { type EditComponentForPattern } from '../../FormManager/FormEdit/types';
 
 export const defaultPatternEditComponents: EditComponentForPattern = {
+  paragraph: ParagraphPatternEdit,
   input: InputPatternEdit,
+  'form-summary': FormSummaryEdit,
   sequence: SequencePatternEdit,
   'submission-confirmation': SubmissionConfirmationEdit,
 };
