b80b20d2980461e9c328efd7c1866ec761cb0f3b
Daniel Naab <dan@crushingpennies.com>
2024-04-29 11:56:45 -0500

    Tweak mobile display of top nav

--- packages/design/src/FormManager/FormManagerLayout/TopNavigation.tsx
+++ packages/design/src/FormManager/FormManagerLayout/TopNavigation.tsx
@@ -42,96 +42,93 @@ export const TopNavigation = ({
   const lastSaved = useFormManagerStore(state => state.lastSaved);
   return (
     <div className="position-sticky top-0 z-100 bg-white padding-1">
-      <div className="grid-container margin-bottom-05 display-block tablet:display-none">
-        <MyFormsLink uswdsRoot={uswdsRoot} />
-        <span className="text-base font-ui-3xs padding-left-4 padding-right-3">
-          Saved
-        </span>
-        {preview && <PreviewIconLink url={preview} uswdsRoot={uswdsRoot} />}
+      <div className="grid-container grid-row margin-bottom-05 display-block tablet:display-none">
+        <div className="grid-row">
+          <div className="grid-col-6">
+            <MyFormsLink uswdsRoot={uswdsRoot} />
+          </div>
+          <div className="grid-col-6 text-base text-right font-ui-3xs padding-left-4">
+            <span className="padding-right-2">Saved</span>
+            {preview && <PreviewIconLink url={preview} uswdsRoot={uswdsRoot} />}
+          </div>
+        </div>
         <MobileStepIndicator />
       </div>
-      <div className="grid-container margin-top-2 display-none tablet:display-block">
-        <div
-          className="usa-step-indicator margin-bottom-105 margin-top-105"
-          aria-label="progress"
-        >
-          <ol className="usa-step-indicator__segments">
-            <li className="margin-top-1">
-              <MyFormsLink uswdsRoot={uswdsRoot} />
-            </li>
-            <li
-              className={classNames(
-                'usa-step-indicator__segment',
-                stepClass(NavPage.upload, curPage)
-              )}
-            >
-              <span className="usa-step-indicator__segment-label">
-                Upload {srHint(NavPage.upload, curPage)}
-              </span>
-            </li>
-            <li
-              className={classNames(
-                'usa-step-indicator__segment',
-                stepClass(NavPage.create, curPage)
-              )}
-            >
-              <span className="usa-step-indicator__segment-label">
-                Create {srHint(NavPage.create, curPage)}
-              </span>
-            </li>
-            <li
-              className={classNames(
-                'usa-step-indicator__segment',
-                stepClass(NavPage.configure, curPage)
-              )}
-              aria-current="true"
-            >
-              <span className="usa-step-indicator__segment-label">
-                Configure {srHint(NavPage.configure, curPage)}
-              </span>
-            </li>
-            <li
-              className={classNames(
-                'usa-step-indicator__segment',
-                stepClass(NavPage.publish, curPage)
-              )}
-            >
-              <span className="usa-step-indicator__segment-label">
-                Publish {srHint(NavPage.publish, curPage)}
-              </span>
-            </li>
-            <li>
-              <span className="text-base font-ui-3xs padding-left-4 padding-right-3">
-                {lastSaved
-                  ? 'Saved at ' +
-                    lastSaved.toLocaleDateString('en-us', {
-                      month: 'short',
-                      day: 'numeric',
-                      hour: 'numeric',
-                      minute: 'numeric',
-                      second: 'numeric',
-                    })
-                  : 'Blueprint loaded'}
-              </span>
-              {preview && (
-                <a href={preview} className="usa-button usa-button--outline">
-                  Preview
-                </a>
-              )}
-            </li>
-          </ol>
-          {/*<ul className="grid-row">
-        {(isPreviewPage || isEditPage || isImportDocuments) && (
-          <li className={`grid-col ${isEditPage ? 'currentPage' : ''}`}>
-            <Link to={`/${formId}/create`}>
-              {isImportDocuments ? 'Back to Edit Page' : 'Edit'}
-            </Link>
-          </li>
-        )}
-        <li className="grid-col">
-          <Link to="/">View all Forms</Link>
-        </li>
-      </ul>*/}
+      <div className="display-none tablet:display-block">
+        <div className="grid-container grid-row">
+          <div
+            className="grid-col-12 margin-bottom-0 usa-step-indicator"
+            aria-label="progress"
+          >
+            <ol className="usa-step-indicator__segments">
+              <li className="margin-right-1 margin-top-1">
+                <MyFormsLink uswdsRoot={uswdsRoot} />
+              </li>
+              <li
+                className={classNames(
+                  'usa-step-indicator__segment',
+                  stepClass(NavPage.upload, curPage)
+                )}
+              >
+                <span className="usa-step-indicator__segment-label">
+                  Upload {srHint(NavPage.upload, curPage)}
+                </span>
+              </li>
+              <li
+                className={classNames(
+                  'usa-step-indicator__segment',
+                  stepClass(NavPage.create, curPage)
+                )}
+              >
+                <span className="usa-step-indicator__segment-label">
+                  Create {srHint(NavPage.create, curPage)}
+                </span>
+              </li>
+              <li
+                className={classNames(
+                  'usa-step-indicator__segment',
+                  stepClass(NavPage.configure, curPage)
+                )}
+                aria-current="true"
+              >
+                <span className="usa-step-indicator__segment-label">
+                  Configure {srHint(NavPage.configure, curPage)}
+                </span>
+              </li>
+              <li
+                className={classNames(
+                  'usa-step-indicator__segment',
+                  stepClass(NavPage.publish, curPage)
+                )}
+              >
+                <span className="usa-step-indicator__segment-label">
+                  Publish {srHint(NavPage.publish, curPage)}
+                </span>
+              </li>
+              <li>
+                <span className="text-base font-ui-3xs padding-left-1 padding-top-2">
+                  {lastSaved
+                    ? 'Saved ' +
+                      lastSaved.toLocaleDateString('en-us', {
+                        month: 'short',
+                        day: 'numeric',
+                        hour: 'numeric',
+                        minute: 'numeric',
+                        second: 'numeric',
+                      })
+                    : 'Blueprint loaded'}
+                </span>
+                {preview && (
+                  <a
+                    href={preview}
+                    className="usa-button usa-button--outline margin-left-1"
+                  >
+                    Preview
+                  </a>
+                )}
+              </li>
+            </ol>
+          </div>
         </div>
       </div>
     </div>
@@ -139,7 +136,7 @@ export const TopNavigation = ({
 };
 
 const MyFormsLink = ({ uswdsRoot }: { uswdsRoot: `${string}/` }) => (
-  <a href={MyForms.getUrl()} className="usa-link margin-right-4">
+  <a href={MyForms.getUrl()} className="usa-link margin-top-3">
     <svg
       className="usa-icon usa-icon--size-3 text-middle"
       aria-hidden="true"
@@ -162,7 +159,7 @@ const PreviewIconLink = ({
   return (
     <a
       href={url}
-      className="usa-link margin-right-4"
+      className="usa-link tablet:margin-right-4"
       aria-label="Preview this blueprint"
     >
       <svg className="usa-icon" aria-hidden="true" focusable="false" role="img">
