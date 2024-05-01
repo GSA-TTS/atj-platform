54469aa957cfa3cc6470b2b9aa75cf0a977be277
Daniel Naab <dan@crushingpennies.com>
2024-05-01 09:01:21 -0500

    Fix case mismatch - CheckBox -> Checkbox

--- packages/design/src/Form/components/index.tsx
+++ packages/design/src/Form/components/index.tsx
@@ -1,7 +1,7 @@
 import { type ComponentForPattern } from '..';
 
 import Address from './Address';
-import CheckboxPattern from './Checkbox';
+import Checkbox from './Checkbox';
 import Fieldset from './Fieldset';
 import FormSummary from './FormSummary';
 import Paragraph from './Paragraph';
@@ -12,7 +12,7 @@ import TextInput from './TextInput';
 
 export const defaultPatternComponents: ComponentForPattern = {
   address: Address,
-  checkbox: CheckboxPattern,
+  checkbox: Checkbox,
   fieldset: Fieldset,
   'form-summary': FormSummary,
   input: TextInput,
