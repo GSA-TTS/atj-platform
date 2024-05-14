c9fffacbd592e50e81ec57006296a72a7295b029
ethangardner <ethan.gardner@gsa.gov>
2024-05-14 14:00:29 -0400

    Added error handling story to CheckboxPatternEdit.stories.tsx
    The commit includes a new user story in CheckboxPatternEdit.stories.tsx demonstrating how the program handles errors when no input is entered into a checkbox pattern field. It triggers this scenario by clearing the field and asserts that it displays an error prompting for at least one character.

--- packages/design/src/FormManager/FormEdit/components/CheckboxPatternEdit.stories.tsx
+++ packages/design/src/FormManager/FormEdit/components/CheckboxPatternEdit.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { userEvent } from '@storybook/test';
+import { expect, userEvent } from '@storybook/test';
 import { within } from '@testing-library/react';
 
 import { type CheckboxPattern } from '@atj/forms/src/patterns/checkbox';
@@ -37,5 +37,27 @@ export const Basic: StoryObj<typeof CheckboxPatternEdit> = {
     const input = canvas.getByLabelText('Field label');
     await userEvent.clear(input);
     await userEvent.type(input, 'Updated checkbox pattern{enter}');
+  },
+};
+
+const mustContainCharacterErrorText =
+  'String must contain at least 1 character(s)';
+export const Error: StoryObj<typeof CheckboxPatternEdit> = {
+  play: async ({ canvasElement }) => {
+    userEvent.setup();
+
+    const canvas = within(canvasElement);
+
+    // Give focus to the field matching `currentLabel`
+    await userEvent.click(await canvas.findByLabelText('Checkbox pattern'));
+
+    // Enter new text for first field
+    const input = canvas.getByLabelText('Field label');
+    await userEvent.clear(input);
+    input.blur();
+
+    await expect(
+      await canvas.findByText(mustContainCharacterErrorText)
+    ).toBeInTheDocument();
   },
 };
