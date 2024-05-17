9fa854a42aa1d4f13873acc8090cc7115421e739
ethangardner <ethan.gardner@gsa.gov>
2024-05-17 11:46:06 -0400

    Add localization and accessibility improvements to RadioGroupPatternEdit

--- packages/design/src/FormManager/FormEdit/components/RadioGroupPatternEdit.tsx
+++ packages/design/src/FormManager/FormEdit/components/RadioGroupPatternEdit.tsx
@@ -10,6 +10,7 @@ import { PatternEditComponent } from '../types';
 import { PatternEditActions } from './common/PatternEditActions';
 import { PatternEditForm } from './common/PatternEditForm';
 import { usePatternEditFormContext } from './common/hooks';
+import { en as message } from '@atj/common/src/locales/en/app';
 
 const RadioGroupPatternEdit: PatternEditComponent<RadioGroupProps> = ({
   focus,
@@ -44,7 +45,7 @@ const EditComponent = ({ pattern }: { pattern: RadioGroupPattern }) => {
           })}
           htmlFor={fieldId('label')}
         >
-          Radio group label
+          {message.patterns.radioGroup.fieldLabel}
         </label>
         {label.error ? (
           <span className="usa-error-message" role="alert">
@@ -83,12 +84,14 @@ const EditComponent = ({ pattern }: { pattern: RadioGroupPattern }) => {
                   id={fieldId(`options.${index}.id`)}
                   {...register(`options.${index}.id`)}
                   defaultValue={option.id}
+                  aria-label={`options.${index}.id value`}
                 />
                 <input
                   className="usa-input"
                   id={fieldId(`options.${index}.label`)}
                   {...register(`options.${index}.label`)}
                   defaultValue={option.label}
+                  aria-label={`options.${index}.id label`}
                 />
               </div>
             </div>
