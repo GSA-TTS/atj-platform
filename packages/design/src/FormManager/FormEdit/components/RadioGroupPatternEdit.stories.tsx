5d71e85d1f648746c00f2a8826b663f1ae94d286
ethangardner <ethan.gardner@gsa.gov>
2024-05-17 12:18:46 -0400

    test error states for radio group label and option label/values

--- packages/design/src/FormManager/FormEdit/components/RadioGroupPatternEdit.stories.tsx
+++ packages/design/src/FormManager/FormEdit/components/RadioGroupPatternEdit.stories.tsx
@@ -4,12 +4,16 @@ import { type RadioGroupPattern } from '@atj/forms/src/patterns/radio-group';
 
 import { createPatternEditStoryMeta } from './common/story-helper';
 import FormEdit from '..';
+import CheckboxPatternEdit from './CheckboxPatternEdit';
+import { en as message } from '@atj/common/src/locales/en/app';
+import { expect, userEvent } from '@storybook/test';
+import { within } from '@testing-library/react';
 
 const pattern: RadioGroupPattern = {
   id: '1',
   type: 'radioGroup',
   data: {
-    label: 'Radio group label',
+    label: message.patterns.radioGroup.displayName,
     options: [
       { label: 'Option 1', id: '1' },
       { label: 'Option 2', id: '2' },
@@ -25,4 +29,57 @@ export default {
   }),
 } as Meta<typeof FormEdit>;
 
-export const Basic: StoryObj<typeof FormEdit> = {};
+export const Basic: StoryObj<typeof FormEdit> = {
+  play: async ({ canvasElement }) => {},
+};
+
+export const Error: StoryObj<typeof CheckboxPatternEdit> = {
+  play: async ({ canvasElement }) => {
+    userEvent.setup();
+
+    const canvas = within(canvasElement);
+
+    await userEvent.click(
+      canvas.getByText(message.patterns.radioGroup.displayName)
+    );
+
+    const input = canvas.getByLabelText(message.patterns.radioGroup.fieldLabel);
+    const optionVal = canvas.getByLabelText('Option 1 id');
+    const optionText = canvas.getByLabelText('Option 1 label');
+
+    // Clear input, remove focus, and wait for error
+    await userEvent.clear(input);
+    input.blur();
+
+    await expect(
+      await canvas.findByText(
+        message.patterns.radioGroup.errorTextMustContainChar
+      )
+    ).toBeInTheDocument();
+
+    /*
+      Repopulate the input value since the error text is indistinguishable from
+      the error text from the option label below
+    */
+    await userEvent.type(input, message.patterns.radioGroup.fieldLabel);
+
+    await userEvent.clear(optionVal);
+    optionVal.blur();
+
+    await expect(
+      await canvas.findByText('Invalid option ID')
+    ).toBeInTheDocument();
+
+    await userEvent.clear(optionText);
+    optionText.blur();
+
+    await expect(
+      await canvas.findByText(
+        message.patterns.radioGroup.errorTextMustContainChar
+      )
+    ).toBeInTheDocument();
+
+    await userEvent.clear(input);
+    input.blur();
+  },
+};
