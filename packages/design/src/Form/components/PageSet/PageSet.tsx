3b3bf2597b0cae4d9c1f631ede401672746b58db
Daniel Naab <dan@crushingpennies.com>
2024-05-24 13:03:19 -0500

    Linting

--- packages/design/src/Form/components/PageSet/PageSet.tsx
+++ packages/design/src/Form/components/PageSet/PageSet.tsx
@@ -1,4 +1,3 @@
-import classNames from 'classnames';
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 
--- packages/forms/src/documents/document.ts
+++ packages/forms/src/documents/document.ts
@@ -115,7 +115,7 @@ export const addDocumentFieldsToForm = (
           maxLength: 128,
         },
       } satisfies InputPattern);
-    } else if (field.type === 'radio-group') {
+    } else if (field.type === 'RadioGroup') {
       patterns.push({
         type: 'input',
         id: patternId,
