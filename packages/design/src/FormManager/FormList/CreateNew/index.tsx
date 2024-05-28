4e5cb5564adaa50a9099cc16b44e022edefa0ecb
ethangardner <ethan.gardner@gsa.gov>
2024-05-28 16:15:59 -0400

    component cleanup

--- packages/design/src/FormManager/FormList/CreateNew/index.tsx
+++ packages/design/src/FormManager/FormList/CreateNew/index.tsx
@@ -169,57 +169,6 @@ export default function CreateNew() {
           </ul>
         </details>
       </div>
-      {false && (
-        <div className="usa-form-group">
-          <button
-            className="usa-button"
-            onClick={async () => {
-              const result = await actions.createNewForm();
-              if (result.success) {
-                navigate(`/${result.data}/upload`);
-              }
-            }}
-          >
-            New form
-          </button>
-          <label
-            className="usa-label"
-            id="file-input-specific-hint"
-            htmlFor="file-input-specific"
-          >
-            Select a single PDF file
-          </label>
-          <div className="usa-file-input">
-            <div className="usa-sr-only" aria-live="polite">
-              No files selected.
-            </div>
-            <div className="usa-file-input__target">
-              <div className="usa-file-input__box"></div>
-              <div className="usa-file-input__instructions" aria-hidden="true">
-                Drag file here or{' '}
-                <span className="usa-file-input__choose">
-                  choose from folder
-                </span>
-              </div>
-
-              <input
-                className="usa-file-input__input"
-                id="file-input-specific"
-                aria-describedby="file-input-specific-hint"
-                type="file"
-                accept=".pdf"
-                onChange={onFileInputChangeGetFile(async fileDetails => {
-                  const result =
-                    await actions.createNewFormByPDFUpload(fileDetails);
-                  if (result.success) {
-                    navigate(`/${result.data}/create`);
-                  }
-                })}
-              />
-            </div>
-          </div>
-        </div>
-      )}
     </>
   );
 }
