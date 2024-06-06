8baa7a5c334f50bec60f5dc966c9e3a6f880d4da
Daniel Naab <dan@crushingpennies.com>
2024-06-06 12:11:26 -0500

    Move the edit pattern save button to the pattern edit actions component.

--- packages/design/src/FormManager/FormEdit/components/FormSummaryEdit.tsx
+++ packages/design/src/FormManager/FormEdit/components/FormSummaryEdit.tsx
@@ -8,6 +8,7 @@ import { PatternEditComponent } from '../types';
 
 import { PatternEditForm } from './common/PatternEditForm';
 import { usePatternEditFormContext } from './common/hooks';
+import { PatternEditActions } from './common/PatternEditActions';
 
 const FormSummaryEdit: PatternEditComponent<FormSummaryProps> = ({
   focus,
@@ -32,29 +33,34 @@ const EditComponent = ({ pattern }: { pattern: Pattern }) => {
   const { register } = usePatternEditFormContext(patternId);
 
   return (
-    <div className="grid-row grid-gap-1">
-      <div className="desktop:grid-col-6 mobile:grid-col-12">
-        <label className="usa-label">
-          Title
-          <input
-            className="usa-input bg-primary-lighter text-bold"
-            {...register('title')}
-            defaultValue={pattern.data.title}
-            type="text"
-          ></input>
-        </label>
+    <>
+      <div className="grid-row grid-gap-1">
+        <div className="desktop:grid-col-6 mobile:grid-col-12">
+          <label className="usa-label">
+            Title
+            <input
+              className="usa-input bg-primary-lighter text-bold"
+              {...register('title')}
+              defaultValue={pattern.data.title}
+              type="text"
+            ></input>
+          </label>
+        </div>
+        <div className="desktop:grid-col-6 mobile:grid-col-12">
+          <label className="usa-label">
+            Description
+            <textarea
+              className="usa-textarea bg-primary-lighter text-bold"
+              {...register('description')}
+              defaultValue={pattern.data.description}
+            ></textarea>
+          </label>
+        </div>
       </div>
-      <div className="desktop:grid-col-6 mobile:grid-col-12">
-        <label className="usa-label">
-          Description
-          <textarea
-            className="usa-textarea bg-primary-lighter text-bold"
-            {...register('description')}
-            defaultValue={pattern.data.description}
-          ></textarea>
-        </label>
+      <div className="grid-col-12">
+        <PatternEditActions />
       </div>
-    </div>
+    </>
   );
 };
 
