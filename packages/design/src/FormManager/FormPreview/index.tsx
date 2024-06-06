327d02f563ede39ec6d275c985203f89535c0c61
Daniel Naab <dan@crushingpennies.com>
2024-06-05 22:36:23 -0500

    Page navigation working on preview page.

--- packages/design/src/FormManager/FormPreview/index.tsx
+++ packages/design/src/FormManager/FormPreview/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { mergeSession } from '@atj/forms';
 
 import Form from '../../Form';
 import { useFormManagerStore } from '../store';
+import { useRouteParams } from '../../FormRouter/hooks';
 
 export const FormPreview = () => {
-  const context = useFormManagerStore(state => state.context);
+  const { context, setSession } = useFormManagerStore(state => ({
+    context: state.context,
+    setSession: state.setSession,
+  }));
   const session = useFormManagerStore(state => state.session);
+  const { routeParams } = useRouteParams();
+
+  useEffect(() => {
+    if (routeParams.page !== session.routeParams?.page) {
+      const newSession = mergeSession(session, { routeParams });
+      setSession(newSession);
+    }
+  }, [routeParams]);
+
   return <Form isPreview={true} context={context} session={session} />;
 };
