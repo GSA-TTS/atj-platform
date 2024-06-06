2fe74c19ce88a00fb04a121763d7e2dbb964093a
Natasha Pierre-Louis <natasha.pierre-louis@gsa.gov>
2024-06-06 13:12:31 -0600

    Added content to the configure and publish pages

--- packages/design/src/FormManager/index.tsx
+++ packages/design/src/FormManager/index.tsx
@@ -26,6 +26,7 @@ import AvailableFormList, {
   UrlForForm,
   UrlForFormManager,
 } from '../AvailableFormList';
+import styles from './FormEdit/formEditStyles.module.css';
 
 export type FormManagerContext = {
   baseUrl: `${string}/`;
@@ -183,7 +184,24 @@ export default function FormManager({ context }: FormManagerProps) {
                   next={AppRoutes.Publish.getUrl(formId)}
                   preview={AppRoutes.Preview.getUrl(formId)}
                 >
-                  Publish
+                  <div className={`${styles.progressPage} grid-container`}>
+                    <div className="grid-row">
+                      <div className="grid-col-12">
+                        <h2>Form Approval and Settings</h2>
+                        <h3>Work in Progress</h3>
+                        <p>On this page, you can add additional settings to your form, gather and review feedback by colleagues and share the form for supervisor review.</p>
+                        <p>Unprioritized Upcoming Feature List:
+                        </p>
+                        <ul className="usa-list">
+                          <li>Commenting tools for colleagues</li>
+                          <li>Commenting and review tools for supervisors</li>
+                          <li>Tracking form edit changes by multiple form builders</li>
+                          <li>Form branding tools</li>
+                          <li>Guided process to include additional forms in the form application</li>
+                        </ul>
+                      </div>
+                    </div>                 
+                  </div>
                 </FormManagerLayout>
               </FormManagerProvider>
             );
@@ -213,7 +231,20 @@ export default function FormManager({ context }: FormManagerProps) {
                   close={AppRoutes.MyForms.getUrl()}
                   preview={AppRoutes.Preview.getUrl(formId)}
                 >
-                  Publish
+                  <div className={`${styles.progressPage} grid-container`}>
+                    <div className="grid-row">
+                      <div className="grid-col-12">
+                        <h2>Publish</h2>
+                        <p>On this page, you can review the pdf preview of your created form and publish it. The exact contents of this page are subject to change following additional user research.</p>
+                        <p>Unprioritized Upcoming Feature List:
+                        </p>
+                        <ul className="usa-list">
+                          <li>A scrollable pdf view</li>
+                          <li>A publish option for the page</li>
+                        </ul>
+                      </div>
+                    </div>                 
+                  </div>
                 </FormManagerLayout>
               </FormManagerProvider>
             );
