a790b761099f01e0846e466d0eb1a61e11de9eab
Daniel Naab <daniel.naab@gsa.gov>
2024-06-17 10:59:36 -0500

    Acquire Cloud.gov secrets from AWS parameter store via a Terraform data lookup. (#189)
    Context: this allows us to easily have separate service accounts for each deployment environment, and rotate credentials.

--- infra/src/lib/app-stack.ts
+++ infra/src/lib/app-stack.ts
@@ -1,9 +1,12 @@
 import { App, TerraformStack } from 'cdktf';
 import { Construct } from 'constructs';
+
+import { AwsProvider } from '../../.gen/providers/aws/provider';
 import { CloudfoundryProvider } from '../../.gen/providers/cloudfoundry/provider';
 
 import { withBackend } from './backend';
 import { CloudGovSpace } from './cloud.gov/space';
+import { DataAwsSsmParameter } from '../../.gen/providers/aws/data-aws-ssm-parameter';
 
 export const registerAppStack = (stackPrefix: string) => {
   const app = new App();
@@ -16,18 +19,35 @@ class AppStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    /*new AwsProvider(this, 'AWS', {
+    new AwsProvider(this, 'AWS', {
       region: 'us-east-2',
-    });*/
+    });
+
+    const cfUsername = new DataAwsSsmParameter(
+      this,
+      `${id}-cloudfoundry-username`,
+      {
+        name: `/${id}/cloudfoundry/username`,
+      }
+    );
+    const cfPassword = new DataAwsSsmParameter(
+      this,
+      `${id}-cloudfoundry-password`,
+      {
+        name: `/${id}/cloudfoundry/password`,
+      }
+    );
 
     new CloudfoundryProvider(this, 'cloud-gov', {
       apiUrl: 'https://api.fr.cloud.gov',
       appLogsMax: 30,
-      ssoPasscode: '',
+      user: cfUsername.value,
+      password: cfPassword.value,
     });
 
-    //new Docassemble(this, `${id}-docassemble`);
     new CloudGovSpace(this, id);
+
+    //new Docassemble(this, `${id}-docassemble`);
     //new FormService(this, `${id}-rest-api`);
   }
 }
