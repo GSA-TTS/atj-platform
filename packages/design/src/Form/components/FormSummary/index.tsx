e5a01c51638fd13e17eb33a15f3fb06e311120a5
Ethan Gardner <ethangardner@users.noreply.github.com>
2024-06-18 16:18:10 -0400

    Full width UI (#206)
    * Use full screen widths in UI. Add max-width constraint to Paragraph and form description
    
    * use class for formslist table instead of overriding uswds usa-table

--- packages/design/src/Form/components/FormSummary/index.tsx
+++ packages/design/src/Form/components/FormSummary/index.tsx
@@ -6,7 +6,7 @@ import { type PatternComponent } from '../../../Form';
 const FormSummary: PatternComponent<FormSummaryProps> = props => {
   return (
     <>
-      <div className="usa-legend-wrapper">
+      <div className="usa-legend-wrapper maxw-tablet">
         {/* <legend className="usa-legend">{pattern.title}</legend> */}
         <h1>{props.title}</h1>
         {props.description !== '' && <p>{props.description}</p>}
