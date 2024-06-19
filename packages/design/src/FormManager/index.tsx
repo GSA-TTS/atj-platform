3f5540ed6dc4cba5fe720e0e2c8e3299fcea5869
Ethan Gardner <ethangardner@users.noreply.github.com>
2024-06-19 10:59:51 -0400

    format code with prettier (#216)

--- packages/design/src/FormManager/index.tsx
+++ packages/design/src/FormManager/index.tsx
@@ -189,28 +189,64 @@ export default function FormManager({ context }: FormManagerProps) {
                       <div className="grid-col-12">
                         <h1 className="font-body-3xl text-center bg-primary-darker padding-3 text-base-lightest">
                           <span>Coming Soon..</span>
-                          <svg className="usa-icon" aria-hidden="true" focusable="false" role="img">
-                            <use xlinkHref={`${context.uswdsRoot}img/sprite.svg#construction_worker`}></use>
+                          <svg
+                            className="usa-icon"
+                            aria-hidden="true"
+                            focusable="false"
+                            role="img"
+                          >
+                            <use
+                              xlinkHref={`${context.uswdsRoot}img/sprite.svg#construction_worker`}
+                            ></use>
                           </svg>
                         </h1>
-                        <h2 className="font-ui-xl">Form Approval and Settings</h2>
+                        <h2 className="font-ui-xl">
+                          Form Approval and Settings
+                        </h2>
                         <h3 className="font-ui-lg">
-                          <svg className="usa-icon display-inline-block text-ttop margin-right-1 text-accent-warm" aria-hidden="true" focusable="false" role="img">
-                            <use xlinkHref={`${context.uswdsRoot}img/sprite.svg#warning`}></use>
+                          <svg
+                            className="usa-icon display-inline-block text-ttop margin-right-1 text-accent-warm"
+                            aria-hidden="true"
+                            focusable="false"
+                            role="img"
+                          >
+                            <use
+                              xlinkHref={`${context.uswdsRoot}img/sprite.svg#warning`}
+                            ></use>
                           </svg>
-                          <span className="display-inline-block text-ttop">Work in Progress - Hello, DOJ Pardon Office!</span>
-                          <svg className="usa-icon display-inline-block text-ttop margin-left-1 text-accent-warm" aria-hidden="true" focusable="false" role="img">
-                            <use xlinkHref={`${context.uswdsRoot}img/sprite.svg#warning`}></use>
+                          <span className="display-inline-block text-ttop">
+                            Work in Progress - Hello, DOJ Pardon Office!
+                          </span>
+                          <svg
+                            className="usa-icon display-inline-block text-ttop margin-left-1 text-accent-warm"
+                            aria-hidden="true"
+                            focusable="false"
+                            role="img"
+                          >
+                            <use
+                              xlinkHref={`${context.uswdsRoot}img/sprite.svg#warning`}
+                            ></use>
                           </svg>
                         </h3>
-                        <p className="font-ui-md">On this page, you can add additional settings to your form, gather and review feedback by colleagues and share the form for supervisor review.</p>
-                        <p className="font-ui-md">Unprioritized Upcoming Feature List:</p>
+                        <p className="font-ui-md">
+                          On this page, you can add additional settings to your
+                          form, gather and review feedback by colleagues and
+                          share the form for supervisor review.
+                        </p>
+                        <p className="font-ui-md">
+                          Unprioritized Upcoming Feature List:
+                        </p>
                         <ul className="usa-list font-ui-md">
                           <li>Commenting tools for colleagues</li>
                           <li>Commenting and review tools for supervisors</li>
-                          <li>Tracking form edit changes by multiple form builders</li>
+                          <li>
+                            Tracking form edit changes by multiple form builders
+                          </li>
                           <li>Form branding tools</li>
-                          <li>Guided process to include additional forms in the form application</li>
+                          <li>
+                            Guided process to include additional forms in the
+                            form application
+                          </li>
                         </ul>
                       </div>
                     </div>
@@ -244,19 +280,32 @@ export default function FormManager({ context }: FormManagerProps) {
                   close={AppRoutes.MyForms.getUrl()}
                   preview={AppRoutes.Preview.getUrl(formId)}
                 >
-
                   <div className={`${styles.progressPage} grid-container`}>
                     <div className="grid-row">
                       <div className="grid-col-12">
                         <h1 className="font-body-3xl text-center bg-primary-darker padding-3 text-base-lightest">
                           <span>Coming Soon..</span>
-                          <svg className="usa-icon" aria-hidden="true" focusable="false" role="img">
-                            <use xlinkHref={`${context.uswdsRoot}img/sprite.svg#construction_worker`}></use>
+                          <svg
+                            className="usa-icon"
+                            aria-hidden="true"
+                            focusable="false"
+                            role="img"
+                          >
+                            <use
+                              xlinkHref={`${context.uswdsRoot}img/sprite.svg#construction_worker`}
+                            ></use>
                           </svg>
                         </h1>
                         <h2 className="font-ui-xl">Publish</h2>
-                        <p className="font-ui-md">On this page, you can review the pdf preview of your created form and publish it. The exact contents of this page are subject to change following additional user research.</p>
-                        <p className="font-ui-md">Unprioritized Upcoming Feature List:</p>
+                        <p className="font-ui-md">
+                          On this page, you can review the pdf preview of your
+                          created form and publish it. The exact contents of
+                          this page are subject to change following additional
+                          user research.
+                        </p>
+                        <p className="font-ui-md">
+                          Unprioritized Upcoming Feature List:
+                        </p>
                         <ul className="usa-list font-ui-md">
                           <li>A scrollable pdf view</li>
                           <li>A publish option for the page</li>
