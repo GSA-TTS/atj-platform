c55eec2e1a6619a4e7923f5a564fdcfe63a12fb0
ethangardner <ethan.gardner@gsa.gov>
2024-06-21 17:16:52 -0400

    Create a test for the add questions and get ready for drag-drop testing once bug is fixed

--- e2e/create.spec.ts
+++ e2e/create.spec.ts
@@ -22,9 +22,14 @@ test('Add questions', async ({ page }) => {
   await page.getByRole('button', { name: 'Short Answer' }).click();
   await menuButton.click();
   await page.getByRole('button', { name: 'Radio Buttons' }).click();
-  const localStorageValue = await page.evaluate((token: string) => localStorage.getItem(token), tokenId[0]);
+  const localStorageValue = await page.evaluate((token: string) => localStorage.getItem(token), (tokenId as Array<string>)[0]);
+  const storageData = JSON.parse((localStorageValue as string));
 
-  console.log(localStorageValue);
-  // {"summary":{"title":"My form - 2024-06-19T01:45:33.643Z","description":""},"root":"root","patterns":{"root":{"type":"page-set","id":"root","data":{"pages":["e8f7f9af-45c8-463f-960f-2f2902ff86f3"]}},"e8f7f9af-45c8-463f-960f-2f2902ff86f3":{"type":"page","id":"e8f7f9af-45c8-463f-960f-2f2902ff86f3","data":{"patterns":["d5ba6957-f0d3-48a0-a503-8b98e014ffb7","3cb8b05a-c9f6-4396-aa6e-06ecf378a29a"]}},"d5ba6957-f0d3-48a0-a503-8b98e014ffb7":{"id":"d5ba6957-f0d3-48a0-a503-8b98e014ffb7","type":"input","data":{"label":"Field label","initial":"","required":true,"maxLength":128}},"3cb8b05a-c9f6-4396-aa6e-06ecf378a29a":{"id":"3cb8b05a-c9f6-4396-aa6e-06ecf378a29a","type":"radio-group","data":{"label":"Radio group label","options":[{"id":"option-1","label":"Option 1"},{"id":"option-2","label":"Option 2"}]}}},"outputs":[]}
+  const firstPage = (storageData.patterns.root.data.pages as Array<string>)[0];
+  const firstPattern = storageData.patterns[firstPage].data.patterns[0];
+  const secondPattern = storageData.patterns[firstPage].data.patterns[1];
+
+  expect(firstPattern).toBe(firstPattern);
+  expect(secondPattern).toBe(secondPattern);
 
 });
