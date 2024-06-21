8cabaad52a0a55d32fce1e2f14b62e16675f1387
ethangardner <ethan.gardner@gsa.gov>
2024-06-21 17:47:42 -0400

    Dry out end to end tests with reusable functions.

--- e2e/create.spec.ts
+++ e2e/create.spec.ts
@@ -1,33 +1,45 @@
-import { test, expect } from '@playwright/test';
-import * as AppRoutes from '../packages/design/src/FormManager/routes';
+import { test, expect, Page } from '@playwright/test';
+import { GuidedFormCreation, Create } from '../packages/design/src/FormManager/routes';
 import { BASE_URL } from './constants';
 
-const uuidPattern = '[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}'
+const uuidPattern = '[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}';
+
+const createNewForm = async (page: Page) => {
+  await page.goto(`${BASE_URL}/${GuidedFormCreation.getUrl()}`);
+  await page.getByRole('button', { name: 'Create New' }).click();
+}
+
+const getLocalStorageData = async (page: Page, token: string) => {
+  const localStorageValue = await page.evaluate((token) => localStorage.getItem(token), token);
+  return JSON.parse((localStorageValue as string));
+};
+
+const getPatternData = (storageData: any, pageNumber: number, patternIndex: number) => {
+  const patternPage = storageData.patterns.root.data.pages[pageNumber];
+  return storageData.patterns[patternPage].data.patterns[patternIndex];
+}
 
 test('Create form from scratch', async ({ page }) => {
-  const createPage = new RegExp(`${uuidPattern}/${AppRoutes.Create.slug}$`, 'i');
-  await page.goto(`${BASE_URL}/${AppRoutes.GuidedFormCreation.getUrl()}`);
-  await page.getByRole('button', { name: 'Create New' }).click();
+  const createPage = new RegExp(`${uuidPattern}/${Create.slug}$`, 'i');
+  await createNewForm(page);
   await expect(page).toHaveURL(createPage);
 });
 
 test('Add questions', async ({ page }) => {
   const formId = new RegExp( `${uuidPattern}`, 'i');
-  await page.goto(`${BASE_URL}/${AppRoutes.GuidedFormCreation.getUrl()}`);
-  await page.getByRole('button', { name: 'Create New' }).click();
+  await createNewForm(page);
 
   const tokenId = page.url().match(formId);
+
   const menuButton = page.getByRole('button', { name: 'Question' });
   await menuButton.click();
   await page.getByRole('button', { name: 'Short Answer' }).click();
   await menuButton.click();
   await page.getByRole('button', { name: 'Radio Buttons' }).click();
-  const localStorageValue = await page.evaluate((token: string) => localStorage.getItem(token), (tokenId as Array<string>)[0]);
-  const storageData = JSON.parse((localStorageValue as string));
 
-  const firstPage = (storageData.patterns.root.data.pages as Array<string>)[0];
-  const firstPattern = storageData.patterns[firstPage].data.patterns[0];
-  const secondPattern = storageData.patterns[firstPage].data.patterns[1];
+  const storageData = await getLocalStorageData(page, (tokenId as Array<any>)[0]);
+  const firstPattern = getPatternData(storageData, 0, 0);
+  const secondPattern = getPatternData(storageData, 0, 1);
 
   expect(firstPattern).toBe(firstPattern);
   expect(secondPattern).toBe(secondPattern);
