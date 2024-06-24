5b3f140023d2e42cae31aaaa90e0ea77107f41ee
Daniel Naab <dan@crushingpennies.com>
2024-06-24 16:22:58 -0500

    App handler serving content, but not 100% working

--- apps/server-doj/src/index.ts
+++ apps/server-doj/src/index.ts
@@ -1,16 +1,9 @@
-import express from 'express';
-//const { handler } = require('@atj/server');
+import { createServer } from '@atj/server/dist/index.js';
 
-const createServer = async () => {
-  // @ts-ignore
-  const { handler } = await import('@atj/server');
-  const app = express();
-  app.use(handler);
-  return app;
-};
-
-const app = await createServer();
-const port = process.env.PORT || 3000;
+const app = await createServer({
+  title: 'DOJ Form Service',
+});
+const port = process.env.PORT || 4321;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,30 @@
-import { handler } from '../dist/server/entry.mjs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 
-export const getHandler = () => {
+import express, { Express } from 'express';
+
+const getDirname = () => {
+  const filePath = fileURLToPath(import.meta.url);
+  return dirname(filePath);
+};
+
+export const getHandler = async () => {
+  // @ts-ignore
+  const { handler } = await import('../dist/server/entry.mjs');
   return handler;
+};
+export type ServerOptions = {
+  title: string;
+};
+
+export const createServer = async (
+  options: ServerOptions
+): Promise<express.Express> => {
+  const app = express();
+  const handler = await getHandler();
+
+  app.use(handler);
+  app.use(express.static(path.join(getDirname(), '../dist/client')));
+
+  return app;
 };
