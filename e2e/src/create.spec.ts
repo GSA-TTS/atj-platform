701d4eb94fbf6a49c48e3e598874fec4d91d109b
Ethan Gardner <ethangardner@users.noreply.github.com>
2024-07-01 10:39:08 -0400

    196 e2e testing (#230)
    * Add Dockerfile for end-to-end testing
    
    A new Dockerfile, Dockerfile.e2e, has been added to set up end-to-end testing in a docker environment. In this Dockerfile, a container with Node.js and playwright preinstalled is initialized and the project dependencies are installed. The README.md has also been updated to include instructions for building and running this Dockerfile in a docker container.
    
    * Iterate on Dockerfile to expose port and limit what is being run on startup
    
    * Add nginx for static file serving
    
    * Use node for file serving
    
    * match docker container version with playwright version
    
    * update readme
    
    * initialize playwright in project
    
    * Add 'slug' property to Route type in FormManager to facilitate testing
    
    * First real e2e test in project
    
    * Change pattern dropdown to use buttons for a11y and e2e testing reasons.
    
    * add constants for testing purposes
    
    * add debugging info for playwright tests
    
    * make uuid pattern regex reusable and build out the add questions test
    
    * rename file
    
    * remove redundant classname
    
    * Create a test for the add questions and get ready for drag-drop testing once bug is fixed
    
    * Dry out end to end tests with reusable functions.
    
    * Add multi-stage build for e2e test docker image.
    
    * update gh actions to include playwright
    
    * update playwright action
    
    * update playwright tests and docker files to set and use env variable
    
    * update playwright test and URL
    
    * remove build step
    
    * update tests with simple 200 check and add instructions to readme
    
    * Move root route test to MyForms file
    
    * Expose port for testing purposes
    
    * remove local storage testing. ci/cd was eating the localStorage window object
    
    * test run on deploy
    
    * update env var name
    
    * Revert "update env var name"
    
    This reverts commit d7dd8b0169196ff8486b22f124095dab32f34b8f.
    
    * Revert "test run on deploy"
    
    This reverts commit 2a4bfb734178bc56be2a1817fa20cb24bd06126a.
    
    * add husky to project
    
    * add test to precommit
    
    * remove example tests file added by playwright scaffolding command
    
    * move files out of project root and into e2e package
    
    * dry up dockerfile
    
    * reorganize docker and tests for e2e
    
    * move playwright config file and update gitignore
    
    * bump up concurrency
    
    * install new dependencies and correct playwright directory
    
    * update gh action
    
    * remove slug from tests
    
    * remove slug from routes

---
+++ e2e/src/create.spec.ts
@@ -0,0 +1,52 @@
+import { test, expect, Page } from '@playwright/test';
+import { GuidedFormCreation, Create } from '../../packages/design/src/FormManager/routes';
+import { BASE_URL } from './constants';
+import { pathToRegexp } from 'path-to-regexp';
+
+
+const createNewForm = async (page: Page) => {
+  await page.goto(`${BASE_URL}/${GuidedFormCreation.getUrl()}`);
+  await page.getByRole('button', { name: 'Create New' }).click();
+}
+
+test('Create form from scratch', async ({ page }) => {
+  const regexp = pathToRegexp(Create.path);
+  await createNewForm(page);
+  let pageUrl = page.url();
+  let pagePath = '';
+
+  if(Create.getUrl().indexOf('#') === 0) {
+    const parts = pageUrl.split('#');
+    if(parts.length === 2) {
+      pagePath = parts[1];
+    }
+  } else {
+    pagePath = new URL(pageUrl).pathname;
+  }
+
+  expect(regexp.test(pagePath)).toBe(true);
+});
+
+test('Add questions', async ({ page }) => {
+  await createNewForm(page);
+
+  const menuButton = page.getByRole('button', { name: 'Question' });
+  await menuButton.click();
+  await page.getByRole('button', { name: 'Short Answer' }).click();
+  await menuButton.click();
+  await page.getByRole('button', { name: 'Radio Buttons' }).click();
+
+  // Create locators for both elements
+  const fields = page.locator('.usa-label');
+  const element1 = fields.filter({ hasText: 'Field Label' })
+  const element2 = fields.filter({ hasText: 'Radio group label' });
+  expect(element1.first()).toBeTruthy();
+  expect(element2.first()).toBeTruthy();
+
+  const htmlContent = await page.content();
+
+  const element1Index = htmlContent.indexOf((await element1.textContent() as string));
+  const element2Index = htmlContent.indexOf((await element2.textContent() as string));
+  expect(element1Index).toBeLessThan(element2Index);
+
+});
