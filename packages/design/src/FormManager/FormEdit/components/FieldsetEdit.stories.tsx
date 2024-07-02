3f7389168aebc05f436c3cd39aa8ba3ad817a58e
Daniel Naab <dan@crushingpennies.com>
2024-07-02 10:54:07 -0500

    Add an interaction test for the fieldset's "add question" button

--- packages/design/src/FormManager/FormEdit/components/FieldsetEdit.stories.tsx
+++ packages/design/src/FormManager/FormEdit/components/FieldsetEdit.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { expect, userEvent } from '@storybook/test';
 import { within } from '@testing-library/react';
 
 import { en as message } from '@atj/common/src/locales/en/app';
@@ -49,5 +50,34 @@ export const Error: StoryObj<typeof FormEdit> = {
       'Legend Text Element',
       message.patterns.fieldset.errorTextMustContainChar
     );
+  },
+};
+
+export const AddPattern: StoryObj<typeof FormEdit> = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    // Add a "short answer" question
+    const addQuestionButton = canvas.getByRole('button', {
+      name: /Add question/,
+    });
+    await userEvent.click(addQuestionButton);
+    const shortAnswerButton = canvas.getByRole('button', {
+      name: /Short answer/,
+    });
+    await userEvent.click(shortAnswerButton);
+
+    // Submit new field's edit form
+    const input = await canvas.findByLabelText('Field label');
+    await userEvent.clear(input);
+    await userEvent.type(input, 'Fieldset short question');
+    const form = input?.closest('form');
+    form?.requestSubmit();
+
+    // Confirm that the "short answer" field exists
+    const updatedElement = await canvas.findAllByText(
+      'Fieldset short question'
+    );
+    await expect(updatedElement.length).toBeGreaterThan(0);
   },
 };
