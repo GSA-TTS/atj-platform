9ebd4f7d8c1c5ebdd8d972c714dd1537ccec8ab9
ethangardner <ethan.gardner@gsa.gov>
2024-07-05 10:21:42 -0400

    test cleanup

--- e2e/src/design/checkbox.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-import { BASE_URL, STORYBOOK_PATH } from '../constants';
-
-test('Page set edit', async ({ page }) => {
-  await page.goto(`${BASE_URL}/${STORYBOOK_PATH}edit-components-pagesetedit--basic`);
-  await expect(page.getByRole('link', { name: 'Page 1 updated' })).toBeVisible();
-});
