5ab94b00b680dcc68716208e2861a5ff976570d7
Ethan Gardner <ethangardner@users.noreply.github.com>
2024-07-12 17:09:54 -0400

    Run storybook tests with playwright (#249)
    * resolve conflicts
    
    * test cleanup
    
    * add post deployment action for testing
    
    * update lockfile to fix failing action
    
    * update action name
    
    * correct config dir path
    
    * add workflow dispatch for testing
    
    * add push action for wip branch
    
    * fix failing test for radio group pattern edit
    
    * update documentation and add script for storybook testing with playwright
    
    * update lockfile
    
    * update path in action
    
    * set endpoint in action
    
    * update workflow endpoint and run mode
    
    * install playwright in gh action
    
    * create a self-contained image that runs through the tests on docker build
    
    * use docker in post-deploy action
    
    * clean up logging for docker build
    
    * simplify end to end tests
    
    * debugging tests
    
    * rename test
    
    * install unfiltered deps in docker--no-verify
    
    * change end-to-end back to imported convention
    
    * test deliberate failure in pipeline
    
    * remove deliberately failing test
    
    * update adr for test strategy
    
    * add drag and drop test
    
    * dry up test code
    
    * add mouse interaction test for drag-and-drop
    
    * fix flaky test
    
    * remove flaky test
    
    * remove commented code
    
    * ignore pnpm cache dir
    
    * use netcat instead of sleep to start server. pare down the copied files in the base image.
    
    * use netcat instead of sleep to start server. pare down the copied files in the base image.
    
    * add script to provide config for docker functions
    
    * update documentation and add provisions for running stories locally
    
    * grammar correction

--- e2e/src/create.spec.ts
+++ e2e/src/create.spec.ts
@@ -5,9 +5,16 @@ import { pathToRegexp } from 'path-to-regexp';
 
 
 const createNewForm = async (page: Page) => {
-  console.log(`${BASE_URL}/${GuidedFormCreation.getUrl()}`);
   await page.goto(`${BASE_URL}/${GuidedFormCreation.getUrl()}`);
   await page.getByRole('button', { name: 'Create New' }).click();
+}
+
+const addQuestions = async (page: Page) => {
+  const menuButton = page.getByRole('button', { name: 'Question' });
+  await menuButton.click();
+  await page.getByRole('button', { name: 'Short Answer' }).click();
+  await menuButton.click();
+  await page.getByRole('button', { name: 'Radio Buttons' }).click();
 }
 
 test('Create form from scratch', async ({ page }) => {
@@ -30,16 +37,11 @@ test('Create form from scratch', async ({ page }) => {
 
 test('Add questions', async ({ page }) => {
   await createNewForm(page);
-
-  const menuButton = page.getByRole('button', { name: 'Question' });
-  await menuButton.click();
-  await page.getByRole('button', { name: 'Short Answer' }).click();
-  await menuButton.click();
-  await page.getByRole('button', { name: 'Radio Buttons' }).click();
+  await addQuestions(page);
 
   // Create locators for both elements
   const fields = page.locator('.usa-label');
-  const element1 = fields.filter({ hasText: 'Field Label' })
+  const element1 = fields.filter({ hasText: 'Field label' });
   const element2 = fields.filter({ hasText: 'Radio group label' });
   expect(element1.first()).toBeTruthy();
   expect(element2.first()).toBeTruthy();
@@ -49,5 +51,26 @@ test('Add questions', async ({ page }) => {
   const element1Index = htmlContent.indexOf((await element1.textContent() as string));
   const element2Index = htmlContent.indexOf((await element2.textContent() as string));
   expect(element1Index).toBeLessThan(element2Index);
+
+});
+
+test('Drag-and-drop via mouse interaction', async ({ page }) => {
+  await createNewForm(page);
+  await addQuestions(page);
+
+  const handle = page.locator('[aria-describedby="DndDescribedBy-0"]').first();
+  await handle.hover();
+  await page.mouse.down();
+  const nextElement = page.locator('.draggable-list-item-wrapper').nth(1);
+  await nextElement.hover();
+  await page.mouse.up();
+
+  // Initiating a reorder clones the element. We want to await the count to go back to 1 to
+  // signify the invocation of the drag event has completed and the update has rendered.
+  await expect(page.getByText('Field label')).toHaveCount(1, {
+    timeout: 10000
+  });
+
+  await expect(page.locator('.draggable-list-item-wrapper').nth(1)).toContainText('Field label');
 
 });
