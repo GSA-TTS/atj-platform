bcff0cfb83a97c65b8010fbc2e39bdd4459da3a2
ethangardner <ethan.gardner@gsa.gov>
2024-07-16 16:16:09 -0400

    provide pagemenuedit in design package root

--- packages/design/src/index.ts
+++ packages/design/src/index.ts
@@ -5,4 +5,5 @@ export { default as Form } from './Form';
 export { defaultPatternComponents } from './Form/components';
 export { default as FormManager, type FormManagerContext } from './FormManager';
 export { defaultPatternEditComponents } from './FormManager/FormEdit/components';
+export { PageMenuEdit } from './FormManager/FormEdit/components/PageMenuEdit';
 export { default as FormRouter } from './FormRouter';
