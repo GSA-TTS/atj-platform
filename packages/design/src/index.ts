181c4b33758cef68aa7de03cc5e5ca86a448b853
ethangardner <ethan.gardner@gsa.gov>
2024-07-24 10:31:00 -0400

    move pagemenuedit component to the pagesetedit file to provide more context of usage

--- packages/design/src/index.ts
+++ packages/design/src/index.ts
@@ -5,5 +5,4 @@ export { default as Form } from './Form';
 export { defaultPatternComponents } from './Form/components';
 export { default as FormManager, type FormManagerContext } from './FormManager';
 export { defaultPatternEditComponents } from './FormManager/FormEdit/components';
-export { PageMenuEdit } from './FormManager/FormEdit/components/PageMenuEdit';
 export { default as FormRouter } from './FormRouter';
