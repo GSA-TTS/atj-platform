bcba4ba0e700605cd3d81c10b9bf72032b15f7eb
Natasha Pierre-Louis <natasha.pierre-louis@gsa.gov>
2024-08-05 22:53:52 -0600

    Ensured that page types can't be moved

--- packages/design/src/FormManager/FormEdit/components/common/PatternEditActions.tsx
+++ packages/design/src/FormManager/FormEdit/components/common/PatternEditActions.tsx
@@ -28,93 +28,93 @@ export const PatternEditActions = ({ children }: PatternEditActionsProps) => {
       p => p.type === 'fieldset' && p.data.patterns.includes(focusPatternId)
     );
   }, [focusPatternId, patterns]);
+
   const isFieldset = focusPatternType === 'fieldset';
+  const isPagePattern = focusPatternType === 'page';
 
   return (
-    <>
+    <div
+      className={`${styles.patternActionWrapper} margin-top-2 margin-bottom-1 padding-top-1 width-full pattern-edit-panel base-dark text-right`}
+    >
       <div
-        className={`${styles.patternActionWrapper} margin-top-2 margin-bottom-1 padding-top-1 width-full pattern-edit-panel base-dark text-right`}
+        className={classNames(
+          'border-top-1px border-bottom-1px border-base-lighter ',
+          {
+            'border-base-lighter': children,
+            'padding-right-1': children,
+            'margin-right-1': children,
+          }
+        )}
       >
-        <div
-          className={classNames(
-            'border-top-1px border-bottom-1px border-base-lighter ',
-            {
-              'border-base-lighter': children,
-              'padding-right-1': children,
-              'margin-right-1': children,
-            }
-          )}
+        {!isPatternInFieldset && !isPagePattern && (
+          <MovePatternDropdown isFieldset={isFieldset} />
+        )}
+        <span
+          className={`${styles.patternActionButtons} margin-top-1 margin-bottom-1 display-inline-block text-ttop`}
         >
-          {!isPatternInFieldset && (
-            <MovePatternDropdown isFieldset={isFieldset} />
-          )}
-          <span
-            className={`${styles.patternActionButtons} margin-top-1 margin-bottom-1 display-inline-block text-ttop`}
-          >
-            <button
-              type="button"
-              aria-label="Create a copy of this pattern"
-              title="Create a copy of this pattern"
-              className="usa-button--outline usa-button--unstyled"
-              onClick={event => {
-                event.preventDefault();
-                alert('Unimplemented');
-              }}
-            >
-              <svg
-                className="usa-icon usa-icon--size-3 margin-1 text-middle"
-                aria-hidden="true"
-                focusable="false"
-                role="img"
-              >
-                <use
-                  xlinkHref={`${context.uswdsRoot}img/sprite.svg#content_copy`}
-                ></use>
-              </svg>
-            </button>
-
-            <button
-              type="button"
-              aria-label="Delete this pattern"
-              title="Delete this pattern"
-              className="usa-button--outline usa-button--unstyled"
-              onClick={event => {
-                event.preventDefault();
-                const confirmed = window.confirm(
-                  'Are you sure you want to delete this question?'
-                );
-                if (confirmed) {
-                  deleteSelectedPattern();
-                }
-              }}
-            >
-              <svg
-                className="usa-icon usa-icon--size-3 margin-1 text-middle"
-                aria-hidden="true"
-                focusable="false"
-                role="img"
-              >
-                <use
-                  xlinkHref={`${context.uswdsRoot}img/sprite.svg#delete`}
-                ></use>
-              </svg>
-            </button>
-            {children ? (
-              <span className="padding-left-1 padding-top-2px">{children}</span>
-            ) : null}
-          </span>
-        </div>
-        <div className="padding-top-2">
           <button
-            type="submit"
-            aria-label="Save and Close"
-            title="Save and Close"
-            className="usa-button"
+            type="button"
+            aria-label="Create a copy of this pattern"
+            title="Create a copy of this pattern"
+            className="usa-button--outline usa-button--unstyled"
+            onClick={event => {
+              event.preventDefault();
+              alert('Unimplemented');
+            }}
           >
-            Save and Close
+            <svg
+              className="usa-icon usa-icon--size-3 margin-1 text-middle"
+              aria-hidden="true"
+              focusable="false"
+              role="img"
+            >
+              <use
+                xlinkHref={`${context.uswdsRoot}img/sprite.svg#content_copy`}
+              ></use>
+            </svg>
           </button>
-        </div>
+
+          <button
+            type="button"
+            aria-label="Delete this pattern"
+            title="Delete this pattern"
+            className="usa-button--outline usa-button--unstyled"
+            onClick={event => {
+              event.preventDefault();
+              const confirmed = window.confirm(
+                'Are you sure you want to delete this question?'
+              );
+              if (confirmed) {
+                deleteSelectedPattern();
+              }
+            }}
+          >
+            <svg
+              className="usa-icon usa-icon--size-3 margin-1 text-middle"
+              aria-hidden="true"
+              focusable="false"
+              role="img"
+            >
+              <use
+                xlinkHref={`${context.uswdsRoot}img/sprite.svg#delete`}
+              ></use>
+            </svg>
+          </button>
+          {children ? (
+            <span className="padding-left-1 padding-top-2px">{children}</span>
+          ) : null}
+        </span>
       </div>
-    </>
+      <div className="padding-top-2">
+        <button
+          type="submit"
+          aria-label="Save and Close"
+          title="Save and Close"
+          className="usa-button"
+        >
+          Save and Close
+        </button>
+      </div>
+    </div>
   );
 };
