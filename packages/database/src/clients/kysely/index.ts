772153932d6056055befe1df91575b74b1a635f3
Daniel Naab <dan@crushingpennies.com>
2024-08-08 20:04:40 -0500

    Fix missing .js extensions on db adapters

--- packages/database/src/clients/kysely/index.ts
+++ packages/database/src/clients/kysely/index.ts
@@ -1,2 +1,2 @@
-export type { Database, DatabaseClient } from './types';
-export { createInMemoryDatabase, createSqliteDatabase } from './sqlite3';
+export type { Database, DatabaseClient } from './types.js';
+export { createInMemoryDatabase, createSqliteDatabase } from './sqlite3.js';
