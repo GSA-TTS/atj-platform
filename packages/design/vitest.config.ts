b1fde84b64facf3a3d718d6341501dcccfea3002
ethangardner <ethan.gardner@gsa.gov>
2024-08-16 18:04:52 -0400

    exclude test that relies on browser feature from vitest runner

--- packages/design/vitest.config.ts
+++ packages/design/vitest.config.ts
@@ -11,6 +11,14 @@ export default mergeConfig(
       ...sharedTestConfig.test,
       environment: 'jsdom',
       setupFiles: './vitest.setup.ts',
+      exclude: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/cypress/**',
+        '**/.{idea,git,cache,output,temp}/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        './src/FormManager/FormEdit/components/RichTextPatternEdit/RichTextPatternEdit.test.tsx',
+      ],
     },
   })
 );
