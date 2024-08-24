9bc5c56e029e2de6b960bf6f6c4529929984f5b1
Daniel Naab <dan@crushingpennies.com>
2024-08-24 00:17:11 -0500

    use createTestFormServiceContext in submitForm test

--- packages/forms/src/services/submit-form.test.ts
+++ packages/forms/src/services/submit-form.test.ts
@@ -1,21 +1,22 @@
 import { describe, expect, it } from 'vitest';
 
 import { createForm, createFormSession } from '../index.js';
-import { createTestBrowserFormService } from '../context/test/index.js';
+import { createTestFormServiceContext } from '../testing.js';
+import { submitForm } from './submit-form.js';
 
 describe('submitForm', () => {
   it('succeeds with empty form', async () => {
-    const service = await createTestBrowserFormService();
+    const ctx = await createTestFormServiceContext({
+      isUserLoggedIn: () => false,
+    });
     const testForm = createForm({ title: 'test', description: 'description' });
-    const addFormResult = await service
-      .getContext()
-      .repository.addForm(testForm);
+    const addFormResult = await ctx.repository.addForm(testForm);
     if (addFormResult.success === false) {
       expect.fail('addForm failed');
     }
     const session = createFormSession(testForm);
 
-    const result = await service.submitForm(session, addFormResult.data.id, {});
+    const result = await submitForm(ctx, session, addFormResult.data.id, {});
     expect(result).toEqual({
       success: true,
       data: [],
