7a9312a4a0f4fe6787158e1a020025b96b69f55e
ethangardner <ethan.gardner@gsa.gov>
2024-10-09 13:54:35 -0400

    update validation to accommodate an array of objects

--- packages/forms/src/patterns/input/response.ts
+++ packages/forms/src/patterns/input/response.ts
@@ -6,11 +6,19 @@ import { safeZodParseToFormError } from '../../util/zod.js';
 import { type InputPattern } from './config.js';
 
 const createSchema = (data: InputPattern['data']) => {
-  const schema = z.string().max(data.maxLength);
-  if (!data.required) {
-    return schema;
-  }
-  return schema.min(1, { message: 'This field is required' });
+  const stringSchema = z.string().max(data.maxLength);
+
+  const baseSchema = data.required
+    ? stringSchema.min(1, { message: 'This field is required' })
+    : stringSchema;
+
+  // Using z.union to handle both single string and object with `repeater` array of strings
+  return z.union([
+    baseSchema,
+    z.object({
+      repeater: z.array(baseSchema),
+    }),
+  ]);
 };
 
 export type InputPatternOutput = z.infer<ReturnType<typeof createSchema>>;
@@ -19,5 +27,9 @@ export const parseUserInput: ParseUserInput<
   InputPattern,
   InputPatternOutput
 > = (pattern, obj) => {
+  // console.group('parseUserInput');
+  // console.log(pattern);
+  // console.log(obj);
+  // console.groupEnd();
   return safeZodParseToFormError(createSchema(pattern['data']), obj);
 };
