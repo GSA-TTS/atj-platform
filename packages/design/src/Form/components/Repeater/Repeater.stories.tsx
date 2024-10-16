142cd3590c89b1bce29b6522848ee9ee4c13db48
ethangardner <ethan.gardner@gsa.gov>
2024-10-16 13:50:26 -0400

    add better tests for repeater component

--- packages/design/src/Form/components/Repeater/Repeater.stories.tsx
+++ packages/design/src/Form/components/Repeater/Repeater.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
-
+import React from 'react';
 import Repeater from './index.js';
+import { expect, userEvent } from '@storybook/test';
 
 export default {
   title: 'patterns/Repeater',
@@ -8,10 +9,44 @@ export default {
   tags: ['autodocs'],
 } satisfies Meta<typeof Repeater>;
 
+const defaultArgs = {
+  legend: 'Default Heading',
+  _patternId: 'test-id',
+};
+
 export const Default = {
   args: {
-    legend: 'Default Heading',
+    ...defaultArgs,
     type: 'repeater',
-    _patternId: 'test-id',
+  },
+} satisfies StoryObj<typeof Repeater>;
+
+export const WithContents = {
+  play: async ({ mount, args }) => {
+    const canvas = await mount(<Repeater {...args} />);
+
+    const addButton = canvas.getByRole('button', { name: /Add new item/ });
+    const deleteButton = canvas.getByRole('button', { name: /Delete item/ });
+    await userEvent.click(addButton);
+
+    let inputs = await canvas.findAllByRole('textbox');
+    await expect(inputs.length).toEqual(2);
+
+    await userEvent.click(deleteButton);
+    inputs = await canvas.findAllByRole('textbox');
+    await expect(inputs.length).toEqual(1);
+  },
+  args: {
+    ...defaultArgs,
+    type: 'repeater',
+    children: [
+      // eslint-disable-next-line
+      <div className="usa-form-group-wrapper">
+        <label className="usa-label" htmlFor="input">
+          Input
+        </label>
+        <input className="usa-input" type="text" id="input" name="input" />
+      </div>,
+    ],
   },
 } satisfies StoryObj<typeof Repeater>;
