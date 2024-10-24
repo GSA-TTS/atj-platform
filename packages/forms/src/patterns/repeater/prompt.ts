2b4eff587b9fb387ff62460e31203dbd0e9d9dca
ethangardner <ethan.gardner@gsa.gov>
2024-10-24 09:53:18 -0400

    wip on repeater field

--- packages/forms/src/patterns/repeater/prompt.ts
+++ packages/forms/src/patterns/repeater/prompt.ts
@@ -12,10 +12,35 @@ export const createPrompt: CreatePrompt<RepeaterPattern> = (
   pattern,
   options
 ) => {
+  console.group('repeater/createPrompt');
+  console.log(session);
+  console.log(pattern);
+  console.groupEnd();
+
   const children = pattern.data.patterns.map((patternId: string) => {
-    const childPattern = getPattern(session.form, patternId);
+    let childPattern = getPattern(session.form, patternId);
+    childPattern = {
+      ...childPattern,
+      id: `${pattern.id}.%%INDEX%%.${childPattern.id}`
+    }
+    // {
+    //   "id": "8c8a358d-e977-4d9b-9671-43bf6e847f5d",
+    //   "type": "input",
+    //   "data": {
+    //   "label": "Field label",
+    //     "initial": "",
+    //     "required": true,
+    //     "maxLength": 128
+    // }
+    // }
+    console.group('repeater/createPrompt/children');
+    console.log(childPattern);
+    console.log(options);
+    console.groupEnd();
     return createPromptForPattern(config, session, childPattern, options);
   });
+
+
   return {
     props: {
       _patternId: pattern.id,
