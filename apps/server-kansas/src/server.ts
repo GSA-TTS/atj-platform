7bcd57f1aa236b76292df5287f3a4fa8f7901fab
Khayal Alasgarov <khayal.alasgarov@gsa.gov>
2024-10-25 14:02:21 -0700

    chore: update server file to add email address for khayal to authorized users list (#355)
    Co-authored-by: Khayal Alasgarov <khayal.alasgarov@gsa.com>

--- apps/server-kansas/src/server.ts
+++ apps/server-kansas/src/server.ts
@@ -19,6 +19,7 @@ export const createCustomServer = async (db: DatabaseContext): Promise<any> => {
         'ethan.gardner@gsa.gov',
         'natasha.pierre-louis@gsa.gov',
         'emily.lordahl@gsa.gov',
+        'khayal.alasgarov@gsa.gov',
       ].includes(email.toLowerCase());
     },
   });
