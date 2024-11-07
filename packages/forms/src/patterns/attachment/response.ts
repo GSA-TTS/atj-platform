a8c4ef34f06ca469d17e1df9862231998fc34e9f
ethangardner <ethan.gardner@gsa.gov>
2024-11-06 20:53:39 -0500

    enhance file input validation for form filler

--- packages/forms/src/patterns/attachment/response.ts
+++ packages/forms/src/patterns/attachment/response.ts
@@ -4,19 +4,52 @@ import { safeZodParseToFormError } from '../../util/zod.js';
 import { type AttachmentPattern } from './index.js';
 
 const createSchema = (data: AttachmentPattern['data']) => {
-  const schema = z
-    .instanceof(FileList)
-    .refine(files => files instanceof FileList, {
-      message: 'The input must be a file',
+  // Define the maximum file size in bytes
+  const maxFileSizeBytes = data.maxFileSizeMB * 1024 * 1024;
+
+  // Define the schema for a single file in the FileList
+  const fileSchema = z
+    .instanceof(File)
+    .refine((file: File) => data.allowedFileTypes.includes(file.type), {
+      message: `Invalid file type`,
+    })
+    .refine((file: File) => file.size <= maxFileSizeBytes, {
+      message: `Each file must be smaller than ${data.maxFileSizeMB} MB`,
     });
 
-  if (data.required) {
-    return schema.refine(files => files.length >= 1, {
-      message: 'At least one file is required',
+  // Define the schema for the FileList as a whole
+  return z
+    .custom<FileList>(
+      (fileList: FileList) => {
+        if (!(fileList instanceof FileList)) {
+          return false;
+        } else if (data.required && fileList.length === 0) {
+          return false;
+        } else if (fileList.length > data.maxAttachments) {
+          return false;
+        }
+        return true;
+      },
+      {
+        message:
+          data.maxAttachments === 1
+            ? `You must provide 1 file`
+            : `You must provide between 1 and ${data.maxAttachments} file(s).`,
+      }
+    )
+    .superRefine((fileList: FileList, ctx: z.RefinementCtx) => {
+      Array.from(fileList).forEach(file => {
+        const result = fileSchema.safeParse(file);
+        if (!result.success) {
+          result.error.errors.forEach((err: z.ZodIssue) => {
+            ctx.addIssue({
+              code: z.ZodIssueCode.custom,
+              message: err.message,
+            });
+          });
+        }
+      });
     });
-  }
-
-  return schema;
 };
 
 export type AttachmentPatternOutput = z.infer<ReturnType<typeof createSchema>>;
@@ -25,13 +58,5 @@ export const parseUserInput: ParseUserInput<
   AttachmentPattern,
   AttachmentPatternOutput
 > = (pattern, obj) => {
-  console.group('parseUserInput');
-  console.group('pattern');
-  console.log(pattern);
-  console.groupEnd();
-  console.group('obj');
-  console.log(obj);
-  console.groupEnd();
-  console.groupEnd();
   return safeZodParseToFormError(createSchema(pattern['data']), obj);
 };
