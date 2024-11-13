db514a6ff7c6dc4bca835ceff628897288e592ef
kalasgarov <khayal.alasgarov@gsa.com>
2024-11-12 17:25:45 -0800

    feat: create phone number pattern tckt-363

---
+++ packages/design/src/Form/components/PhoneNumber/PhoneNumber.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useFormContext } from 'react-hook-form';
+import { type PhoneNumberProps } from '@atj/forms';
+import { type PatternComponent } from '../../index.js';
+
+export const PhoneNumberPattern: PatternComponent<PhoneNumberProps> = ({
+  phoneId,
+  hint,
+  label,
+  required,
+  error,
+  value,
+}) => {
+  const { register } = useFormContext();
+
+  return (
+    <fieldset className="usa-fieldset">
+      <div className="usa-form-group">
+        <label className="usa-label" htmlFor={phoneId}>
+          {label}
+          {required && <span className="required-indicator">*</span>}
+        </label>
+        {hint && (
+          <div className="usa-hint" id="primaryPnHint">
+            {hint}
+          </div>
+        )}
+        <input
+          className="usa-input margin-bottom-1"
+          id={phoneId}
+          type="tel"
+          {...register(phoneId, { required })}
+          defaultValue={value}
+          aria-describedby="primaryPnHint"
+          // pattern="[0-9]*"
+          // inputMode="numeric"
+        />
+        {error && (
+          <div className="error-message" style={{ color: 'red' }}>
+            {error.message}
+          </div>
+        )}
+      </div>
+    </fieldset>
+  );
+};
