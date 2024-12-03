ff57f58a921121b1b96563473c6e5e7ed2ef379b
kalasgarov <khayal.alasgarov@gsa.com>
2024-12-03 14:18:56 -0800

    chore: update folder structrue for form manager tckt-394

---
+++ packages/design/src/FormManager/FormEdit/components/CheckboxPatternEdit/index.tsx
@@ -0,0 +1,89 @@
+import classnames from 'classnames';
+import React from 'react';
+
+import { enLocale as message } from '@atj/common';
+import { type CheckboxProps } from '@atj/forms';
+import { type CheckboxPattern } from '@atj/forms';
+
+import Checkbox from '../../../../Form/components/Checkbox/index.js';
+import { PatternEditComponent } from '../../types.js';
+
+import { PatternEditActions } from '../common/PatternEditActions.js';
+import { PatternEditForm } from '../common/PatternEditForm.js';
+import { usePatternEditFormContext } from '../common/hooks.js';
+
+const CheckboxPatternEdit: PatternEditComponent<CheckboxProps> = ({
+  focus,
+  previewProps,
+}) => {
+  return (
+    <>
+      {focus ? (
+        <PatternEditForm
+          pattern={focus.pattern}
+          editComponent={<CheckboxEditComponent pattern={focus.pattern} />}
+        ></PatternEditForm>
+      ) : (
+        <div className="padding-left-3 padding-bottom-3 padding-right-3">
+          <Checkbox {...previewProps} />
+        </div>
+      )}
+    </>
+  );
+};
+
+const CheckboxEditComponent = ({ pattern }: { pattern: CheckboxPattern }) => {
+  const { fieldId, getFieldState, register } =
+    usePatternEditFormContext<CheckboxPattern>(pattern.id);
+
+  const label = getFieldState('label');
+
+  return (
+    <div className="grid-row grid-gap padding-3">
+      <div className="tablet:grid-col-6 mobile-lg:grid-col-12">
+        <label
+          className={classnames('usa-label', {
+            'usa-label--error': label.error,
+          })}
+          htmlFor={fieldId('label')}
+        >
+          {message.patterns.checkbox.fieldLabel}
+        </label>
+        {label.error ? (
+          <span className="usa-error-message" role="alert">
+            {label.error.message}
+          </span>
+        ) : null}
+        <input
+          className="usa-input"
+          id={fieldId('label')}
+          defaultValue={pattern.data.label}
+          {...register('label')}
+          type="text"
+          autoFocus
+        ></input>
+      </div>
+      <div className="tablet:grid-col-6 mobile-lg:grid-col-12">
+        <div className="usa-checkbox">
+          <input
+            id={fieldId('defaultChecked')}
+            type="checkbox"
+            {...register('defaultChecked')}
+            className="usa-checkbox__input"
+          />
+          <label
+            className="usa-checkbox__label"
+            htmlFor={fieldId('defaultChecked')}
+          >
+            {message.patterns.checkbox.defaultFieldValue}
+          </label>
+        </div>
+      </div>
+      <div className="grid-col-12">
+        <PatternEditActions />
+      </div>
+    </div>
+  );
+};
+
+export default CheckboxPatternEdit;
