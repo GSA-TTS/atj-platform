d1c388f70764ab414f25157d6860b623a3d6ef7d
Khayal Alasgarov <khayal.alasgarov@gsa.gov>
2024-12-06 10:23:20 -0800

    chore: update folder structrue for form manager tckt-394 (#396)
    Co-authored-by: kalasgarov <khayal.alasgarov@gsa.com>

---
+++ packages/design/src/FormManager/FormEdit/components/CheckboxPatternEdit/CheckboxPatternEdit.stories.tsx
@@ -0,0 +1,60 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { type CheckboxPattern } from '@atj/forms';
+
+import CheckboxPatternEdit from './index.js';
+import {
+  createPatternEditStoryMeta,
+  testEmptyFormLabelError,
+  testUpdateFormFieldOnSubmit,
+} from '../common/story-helper.js';
+import FormEdit from '../../index.js';
+import { enLocale as message } from '@atj/common';
+import { expect } from '@storybook/test';
+import { within } from '@testing-library/react';
+
+const pattern: CheckboxPattern = {
+  id: '1',
+  type: 'checkbox',
+  data: {
+    label: message.patterns.checkbox.displayName,
+    defaultChecked: false,
+  },
+};
+
+const storyConfig: Meta<typeof FormEdit> = {
+  title: 'Edit components/CheckboxPatternEdit',
+  ...createPatternEditStoryMeta({
+    pattern,
+  }),
+};
+export default storyConfig;
+
+export const Basic: StoryObj<typeof CheckboxPatternEdit> = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const updatedLabel = 'Updated checkbox pattern';
+
+    await testUpdateFormFieldOnSubmit(
+      canvasElement,
+      message.patterns.checkbox.displayName,
+      message.patterns.checkbox.fieldLabel,
+      updatedLabel
+    );
+
+    await expect(
+      await canvas.findByLabelText(updatedLabel)
+    ).toBeInTheDocument();
+  },
+};
+
+export const Error: StoryObj<typeof CheckboxPatternEdit> = {
+  play: async ({ canvasElement }) => {
+    await testEmptyFormLabelError(
+      canvasElement,
+      message.patterns.checkbox.displayName,
+      message.patterns.checkbox.fieldLabel,
+      message.patterns.checkbox.errorTextMustContainChar
+    );
+  },
+};
---
+++ packages/design/src/FormManager/FormEdit/components/SelectDropdownPatternEdit/index.tsx
@@ -0,0 +1,144 @@
+import classnames from 'classnames';
+import React, { useState } from 'react';
+
+import { type SelectDropdownProps } from '@atj/forms';
+import { type SelectDropdownPattern } from '@atj/forms';
+
+import SelectDropdown from '../../../../Form/components/SelectDropdown/index.js';
+import { PatternEditComponent } from '../../types.js';
+
+import { PatternEditActions } from '../common/PatternEditActions.js';
+import { PatternEditForm } from '../common/PatternEditForm.js';
+import { usePatternEditFormContext } from '../common/hooks.js';
+import { enLocale as message } from '@atj/common';
+import styles from '../../formEditStyles.module.css';
+
+const SelectDropdownPatternEdit: PatternEditComponent<SelectDropdownProps> = ({
+  focus,
+  previewProps,
+}) => {
+  return (
+    <>
+      {focus ? (
+        <PatternEditForm
+          pattern={focus.pattern}
+          editComponent={<EditComponent pattern={focus.pattern} />}
+        ></PatternEditForm>
+      ) : (
+        <div
+          // TODO: update styles for select if available
+          className={`${styles.radioFormPattern} padding-left-3 padding-bottom-3 padding-right-3`}
+        >
+          <SelectDropdown {...previewProps} />
+        </div>
+      )}
+    </>
+  );
+};
+
+const EditComponent = ({ pattern }: { pattern: SelectDropdownPattern }) => {
+  const { fieldId, getFieldState, register } =
+    usePatternEditFormContext<SelectDropdownPattern>(pattern.id);
+  const [options, setOptions] = useState(pattern.data.options);
+  const label = getFieldState('label');
+
+  return (
+    <div className="grid-row grid-gap">
+      <div className="tablet:grid-col-6 mobile-lg:grid-col-12 margin-bottom-2">
+        <label
+          className={classnames('usa-label', {
+            'usa-label--error': label.error,
+          })}
+        >
+          {message.patterns.selectDropdown.fieldLabel}
+          {label.error ? (
+            <span className="usa-error-message" role="alert">
+              {label.error.message}
+            </span>
+          ) : null}
+          <input
+            className="usa-input"
+            id={fieldId('label')}
+            defaultValue={pattern.data.label}
+            {...register('label')}
+            type="text"
+            autoFocus
+          ></input>
+        </label>
+      </div>
+      <div className="tablet:grid-col-6 mobile-lg:grid-col-12">
+        {options.map((option, index) => {
+          const optionValue = getFieldState(`options.${index}.value`);
+          const optionLabel = getFieldState(`options.${index}.label`);
+          return (
+            <div key={index}>
+              {optionValue.error ? (
+                <span className="usa-error-message" role="alert">
+                  {optionValue.error.message}
+                </span>
+              ) : null}
+              {optionLabel.error ? (
+                <span className="usa-error-message" role="alert">
+                  {optionLabel.error.message}
+                </span>
+              ) : null}
+              <div className="display-flex margin-bottom-2">
+                <input
+                  className={classnames('hide', 'usa-input', {
+                    'usa-label--error': label.error,
+                  })}
+                  id={fieldId(`options.${index}.value`)}
+                  {...register(`options.${index}.value`)}
+                  defaultValue={option.value}
+                  aria-label={`Option ${index + 1} value`}
+                />
+                <input
+                  className="usa-input"
+                  id={fieldId(`options.${index}.label`)}
+                  {...register(`options.${index}.label`)}
+                  defaultValue={option.label}
+                  aria-label={`Option ${index + 1} label`}
+                />
+              </div>
+            </div>
+          );
+        })}
+        <button
+          className="usa-button usa-button--outline margin-top-1"
+          type="button"
+          onClick={event => {
+            event.preventDefault();
+            const optionId = `option-${options.length + 1}`;
+            const optionValue = `value-${options.length + 1}`;
+            setOptions(options.concat({ value: optionValue, label: optionId }));
+          }}
+        >
+          Add new
+        </button>
+      </div>
+      <div className="grid-col-12">
+        <PatternEditActions>
+          <span className="usa-checkbox">
+            <input
+              style={{ display: 'inline-block' }}
+              className="usa-checkbox__input bg-primary-lighter"
+              type="checkbox"
+              id={fieldId('required')}
+              {...register('required')}
+              defaultChecked={pattern.data.required}
+            />
+            <label
+              style={{ display: 'inline-block' }}
+              className="usa-checkbox__label"
+              htmlFor={fieldId('required')}
+            >
+              Required
+            </label>
+          </span>
+        </PatternEditActions>
+      </div>
+    </div>
+  );
+};
+
+export default SelectDropdownPatternEdit;
