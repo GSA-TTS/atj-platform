22a74d0ab6a0cbf6707406d188a16974ecfe82e0
ethangardner <ethan.gardner@gsa.gov>
2024-12-09 14:38:08 -0500

    add splash content

---
+++ apps/spotlight/src/components/SplashPage.tsx
@@ -0,0 +1,163 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <main id="main-content">
+      <section className="usa-section usa-section--dark" aria-label="Introduction">
+        <div className="grid-container">
+          <div className="usa-hero__callout">
+            <h1 className="usa-hero__heading">
+              Elegant Federal Forms that Work for All Users on All Devices
+            </h1>
+            <ul className="usa-button-group">
+              <li className="usa-button-group__item">
+                <a href="#" className="usa-button usa-button--outline">Download our Brochure</a>
+              </li>
+              <li className="usa-button-group__item">
+                <a href="#" className="usa-button">Request a Demo</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section className="grid-container usa-section">
+        <div className="grid-row grid-gap">
+          <div className="tablet:grid-col-4">
+            <h2 className="font-heading-xl margin-top-0 tablet:margin-bottom-0">
+              What Makes Us Different
+            </h2>
+          </div>
+          <div className="tablet:grid-col-8 usa-prose">
+            <p>
+              The tagline should inspire confidence and interest, focusing on the
+              value that your overall approach offers to your audience. Use a
+              heading typeface and keep your tagline to just a few words, and
+              don’t confuse or mystify.
+            </p>
+            <p>
+              Use the right side of the grid to explain the tagline a bit more.
+              What are your goals? How do you do your work? Write in the present
+              tense, and stay brief here. People who are interested can find
+              details on internal pages.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="grid-container usa-section--dark">
+        <div className="grid-row grid-gap">
+          <div className="tablet:grid-col-4">
+            <h2 className="font-heading-xl margin-top-0 tablet:margin-bottom-0">
+              Compliance with 21C IDEA and M-23-22 out of the box.
+            </h2>
+          </div>
+          <div className="tablet:grid-col-8 usa-prose">
+            <p>
+              The tagline should inspire confidence and interest, focusing on the
+              value that your overall approach offers to your audience. Use a
+              heading typeface and keep your tagline to just a few words, and
+              don’t confuse or mystify.
+            </p>
+            <p>
+              Use the right side of the grid to explain the tagline a bit more.
+              What are your goals? How do you do your work? Write in the present
+              tense, and stay brief here. People who are interested can find
+              details on internal pages.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="grid-container usa-section">
+        <div className="grid-row grid-gap">
+          <div className="tablet:grid-col-4">
+            <h2 className="font-heading-xl margin-top-0 tablet:margin-bottom-0">
+              Integrate with your Data
+            </h2>
+          </div>
+          <div className="tablet:grid-col-8 usa-prose">
+            <p>
+              The tagline should inspire confidence and interest, focusing on the
+              value that your overall approach offers to your audience. Use a
+              heading typeface and keep your tagline to just a few words, and
+              don’t confuse or mystify.
+            </p>
+            <p>
+              Use the right side of the grid to explain the tagline a bit more.
+              What are your goals? How do you do your work? Write in the present
+              tense, and stay brief here. People who are interested can find
+              details on internal pages.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="grid-container usa-section">
+        <div className="grid-row grid-gap">
+          <div className="tablet:grid-col-4">
+            <h2 className="font-heading-xl margin-top-0 tablet:margin-bottom-0">
+              We Understand your Concerns
+            </h2>
+          </div>
+          <div className="tablet:grid-col-8 usa-prose">
+            <p>
+              The tagline should inspire confidence and interest, focusing on the
+              value that your overall approach offers to your audience. Use a
+              heading typeface and keep your tagline to just a few words, and
+              don’t confuse or mystify.
+            </p>
+            <p>
+              Use the right side of the grid to explain the tagline a bit more.
+              What are your goals? How do you do your work? Write in the present
+              tense, and stay brief here. People who are interested can find
+              details on internal pages.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="grid-container usa-section">
+        <div className="grid-row grid-gap">
+          <div className="tablet:grid-col-4">
+            <h2 className="font-heading-xl margin-top-0 tablet:margin-bottom-0">
+              We’re Gonna Launch the Presidential Pardon Form (Yes, That One)
+            </h2>
+          </div>
+          <div className="tablet:grid-col-8 usa-prose">
+            <p>
+              The tagline should inspire confidence and interest, focusing on the
+              value that your overall approach offers to your audience. Use a
+              heading typeface and keep your tagline to just a few words, and
+              don’t confuse or mystify.
+            </p>
+            <p>
+              Use the right side of the grid to explain the tagline a bit more.
+              What are your goals? How do you do your work? Write in the present
+              tense, and stay brief here. People who are interested can find
+              details on internal pages.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="grid-container usa-section">
+        <div className="grid-row grid-gap">
+          <div className="tablet:grid-col-4">
+            <h2 className="font-heading-xl margin-top-0 tablet:margin-bottom-0">
+              Find Out More About Other 10x Projects
+            </h2>
+          </div>
+          <div className="tablet:grid-col-8 usa-prose">
+            <p>
+              The tagline should inspire confidence and interest, focusing on the
+              value that your overall approach offers to your audience. Use a
+              heading typeface and keep your tagline to just a few words, and
+              don’t confuse or mystify.
+            </p>
+            <p>
+              Use the right side of the grid to explain the tagline a bit more.
+              What are your goals? How do you do your work? Write in the present
+              tense, and stay brief here. People who are interested can find
+              details on internal pages.
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  )
+}
